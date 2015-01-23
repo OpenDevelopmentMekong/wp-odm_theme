8e745dabca67fed54c45d05ba6e0c5bd255921b6
Miguel Peixe <miguel@cardume.art.br>
2015-01-23 14:07:18 -0200

    start data archive page

---
+++ page-data.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: Data archive
+*/
+?>
+<?php get_header(); ?>
+
+<?php if(have_posts()) : the_post(); ?>
+	<section id="content" class="single-post">
+		<header class="single-post-header">
+			<div class="container">
+				<div class="twelve columns">
+					<h1><?php the_title(); ?></h1>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			<div class="eight columns">
+				<?php do_shortcode('[wpckan_query_datasets limit="10"]'); ?>
+			</div>
+			<div class="three columns offset-by-one">
+				<aside id="sidebar">
+					<ul class="widgets">
+						<?php dynamic_sidebar('general'); ?>
+					</ul>
+				</aside>
+			</div>
+		</div>
+	</section>
+<?php endif; ?>
+
+<?php get_footer(); ?>
