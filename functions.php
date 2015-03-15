95c4dffb127ac975b50060f2d64edb74f80c8d07
Mishari Muqb <mishari@mishari.net>
2015-03-15 14:04:19 +0000

    Changed FB button to English, fixes #26

--- functions.php
+++ functions.php
@@ -194,7 +194,7 @@ function opendev_social_apis() {
 	  var js, fjs = d.getElementsByTagName(s)[0];
 	  if (d.getElementById(id)) return;
 	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=1413694808863403";
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1413694808863403";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));</script>
 	<?php
