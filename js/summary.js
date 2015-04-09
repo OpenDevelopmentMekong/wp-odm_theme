93efa4487e8d207f4642b42f9733f3cd3c2cef96
Huy Eng Heng <huyeng_heng@yahoo.com>
2015-04-09 11:51:50 +0700

    Fixs issue #51 related the table content.

--- js/summary.js
+++ js/summary.js
@@ -14,18 +14,22 @@
 		var tags = ['h1','h2','h3','h4','h5','h6'];
 		container = $('.table-of-contents');
 		hItems = content.find('.summary-item');
-		hItems.each(function(index) {
-			$(this).attr('data-count', index);
-		});
-		console.log(hItems);
-		container.append('<ol/>');
-		hItems.each(function() {
-			if($(this).is('.summary-item')) {
-				var item = $('<li/>').addClass($(this).attr('id') + ' ' + $(this).attr('class'));
-				item.append($(this).find('a').clone());
-				container.find('ol').append(item);
-			}
-		});
+		if (hItems.length > 2){ //odm
+			hItems.each(function(index) {
+				$(this).attr('data-count', index);
+			});
+			console.log(hItems);
+			container.append('<ol/>');
+			hItems.each(function() {
+				if($(this).is('.summary-item')) {
+					var item = $('<li/>').addClass($(this).attr('id') + ' ' + $(this).attr('class'));
+					item.append($(this).find('a').clone());
+					container.find('ol').append(item);
+				}
+			});
+		}else{ // odm
+			$('#content .opendev-summary').parent('li').hide();
+		} 
 	}
 
 })(jQuery);
