aae6eda8e12eacb070908be2e9bff423bb9648b9
Alex Corbi <a.corbi@gmail.com>
2015-06-17 17:16:08 +0200

    implementing #85

--- inc/interactive-map.php
+++ inc/interactive-map.php
@@ -32,11 +32,14 @@ class OpenDev_InteractiveMap {
     $layer = array();
     $layer['filtering'] = 'switch';
     $layer['hidden'] = 1;
-    foreach($categories as $cat) {
+    $count = 0;
+    foreach($categories as $key=>$val) {
+    $cat = $categories[$key];
      if(is_object_in_term(get_the_ID(), 'layer-category', $cat->term_id)) {
       if(!isset($parsed_cats[$cat->term_id]))
        $parsed_cats[$cat->term_id] = array();
       $parsed_cats[$cat->term_id][] = get_the_ID();
+      $parsed_cats[$cat->term_id]['order'] = $key;
      }
     }
     $layer = array_merge($layer, jeo_get_layer(get_the_ID()));
@@ -58,22 +61,19 @@ class OpenDev_InteractiveMap {
    ));
   }
 
-  // print_r($map);
-
   ob_start();
   ?>
   <div class="interactive-map">
    <div class="map-container">
     <div id="map_interactive_map_0" class="map"></div>
-    <!-- <div id="interactive-map-layer-info">
-    </div> -->
+
    </div>
    <div class="interactive-map-layers">
     <ul class="categories">
      <?php
       $categories = get_categories('taxonomy=layer-category');
       foreach ($categories as $category){ ?>
-       <li class="<?php echo "cat-item cat-item-" . $category->cat_ID ?>"><a href="#"><?php echo $category->cat_name; ?></a></li>
+       <li draggable="true" data-category="<?php echo $category->cat_ID ?>" class="<?php echo "cat-item cat-item-" . $category->cat_ID ?>"><a href="#"><?php echo $category->cat_name; ?></a></li>
      <?php } ?>
     </ul>
    </div>
@@ -82,23 +82,20 @@ class OpenDev_InteractiveMap {
 
    (function($) {
 
-    // function show_layer_info(layer) {
-    //  var $layer_info = $('#interactive-map-layer-info');
-    //  $layer_info.html('<h2>'+layer+'</h2></br>'+layer);
-    //  $layer_info.show();
-    // }
-    //
-    // function hide_layer_info(layer) {
-    //  var $layer_info = $('#interactive-map-layer-info');
-    //  $layer_info.hide();
-    // }
-
+    var ENABLE_DND = false;
     var dragSrcEl = null;
+    var category_items = document.querySelectorAll('.cat-item');
+
+    function swapLayerCategory(cat1_ID,cat2_ID) {
+      var temp_order = term_rel[cat1_ID]['order'];
+      term_rel[cat1_ID]['order'] = term_rel[cat2_ID]['order'];
+      term_rel[cat2_ID]['order'] = temp_order;
+      jeo_map.filterLayers._update();
+    }
 
     function handleDragStart(e) {
-     console.log("handleDragStart");
+      //console.log("handleDragStart");
      // Target (this) element is the source node.
-     //this.style.opacity = '0.4';
      this.style.cursor = 'move';
      dragSrcEl = this;
 
@@ -107,7 +104,7 @@ class OpenDev_InteractiveMap {
     }
 
     function handleDragOver(e) {
-     console.log("handleDragOver");
+      //console.log("handleDragOver");
      if (e.preventDefault) {
        e.preventDefault(); // Necessary. Allows us to drop.
      }
@@ -118,31 +115,29 @@ class OpenDev_InteractiveMap {
     }
 
     function handleDragEnter(e) {
-     console.log("handleDragEnter");
+      //console.log("handleDragEnter");
      // this / e.target is the current hover target.
      this.classList.add('over');
     }
 
     function handleDragLeave(e) {
-     console.log("handleDragLeave");
-     this.classList.removeClass('over');  // this / e.target is previous target element.
-     //this.style.opacity = '1.0';
+      //console.log("handleDragLeave");
+     this.classList.remove('over');
     }
 
     function handleDrop(e) {
-     console.log("handleDrop");
-     // this/e.target is current target element.
-     //this.style.opacity = '1.0';
+     //console.log("handleDrop");
 
      if (e.stopImmediatePropagation) {
        e.stopImmediatePropagation(); // Stops some browsers from redirecting.
      }
 
-     // Don't do anything if dropping the same column we're dragging.
+     // Don't do anything if dropping the same item we're dragging.
      if (dragSrcEl != this) {
-       // Set the source column's HTML to the HTML of the column we dropped on.
+       // Set the source item's HTML to the HTML of the item we dropped on.
        dragSrcEl.innerHTML = this.innerHTML;
        this.innerHTML = e.dataTransfer.getData('text/html');
+       swapLayerCategory($(this).data('category'),$(dragSrcEl).data('category'));
      }
 
      return false;
@@ -152,64 +147,66 @@ class OpenDev_InteractiveMap {
      console.log("handleDragEnd");
      // this/e.target is the source node.
 
-     [].forEach.call(category_items, function (col) {
-       col.classList.remove('over');
+     [].forEach.call(category_items, function (item) {
+       item.classList.remove('over');
      });
     }
 
     var term_rel = <?php echo json_encode($parsed_cats); ?>;
-
+    var jeo_map;
     jeo(jeo.parseConf(<?php echo json_encode($map); ?>));
 
     jeo.mapReady(function(map) {
 
-     var $layers = $('.interactive-map .interactive-map-layers');
-     if(map.postID == 'interactive_map') {
-      //map.$.find('.jeo-filter-layers').appendTo($layers);
-      for(var key in term_rel) {
-       var $item = $layers.find('.cat-item-' + key);
-       $item.find(' > a').after($('<ul class="cat-layers switch-layers" />'));
-       $.each(term_rel[key], function(i, layerId) {
-        var $layer = map.$.find('[data-layer="' + layerId + '"]');
-        $layer.appendTo($item.find('.cat-layers'));
+       jeo_map = map;
+
+       var $layers = $('.interactive-map .interactive-map-layers');
+       if(map.postID == 'interactive_map') {
+        //map.$.find('.jeo-filter-layers').appendTo($layers);
+        for(var key in term_rel) {
+         var $item = $layers.find('.cat-item-' + key);
+         $item.find(' > a').after($('<ul class="cat-layers switch-layers" />'));
+         $.each(term_rel[key], function(i, layerId) {
+          var $layer = map.$.find('[data-layer="' + layerId + '"]');
+          $layer.appendTo($item.find('.cat-layers'));
+         });
+        }
+        //$('.jeo-filter-layers').hide();
+       }
+
+       $layers.find('.categories ul').hide();
+
+       $layers.find('.categories li a').on('click', function() {
+        if($(this).hasClass('active')) {
+         $(this).removeClass('active');
+         $(this).parent().find('ul').hide();
+        } else {
+         $(this).addClass('active');
+         $(this).parent().find('> ul').show();
+        }
+        return false;
        });
-      }
-      $('.jeo-filter-layers').hide();
+
+       $layers.find('.cat-layers li').on('click', function() {
+        map.filterLayers._switchLayer($(this).data('layer'));
+        if(map.filterLayers._getStatus($(this).data('layer')).on) {
+         $(this).addClass('active');
+        } else {
+         $(this).removeClass('active');
+        }
+
+     });
+
+     if (ENABLE_DND){
+       [].forEach.call(category_items, function(item) {
+        item.addEventListener('dragstart', handleDragStart, false);
+        item.addEventListener('dragenter', handleDragEnter, false);
+        item.addEventListener('dragover', handleDragOver, false);
+        item.addEventListener('dragleave', handleDragLeave, false);
+        item.addEventListener('drop', handleDrop, false);
+        item.addEventListener('dragend', handleDragEnd, false);
+       });
      }
-
-     $layers.find('.categories ul').hide();
-
-     $layers.find('.categories li a').on('click', function() {
-      if($(this).hasClass('active')) {
-       $(this).removeClass('active');
-       $(this).parent().find('ul').hide();
-      } else {
-       $(this).addClass('active');
-       $(this).parent().find('> ul').show();
-      }
-      return false;
-     });
-
-     $layers.find('.cat-layers li').on('click', function() {
-      map.filterLayers._switchLayer($(this).data('layer'));
-      if(map.filterLayers._getStatus($(this).data('layer')).on) {
-       $(this).addClass('active');
-       // show_layer_info($(this).data('layer'));
-      } else {
-       $(this).removeClass('active');
-       // hide_layer_info($(this).data('layer'));
-      }
-     });
-
-     var category_items = document.querySelectorAll('.categories li');
-     // [].forEach.call(category_items, function(item) {
-     //  item.addEventListener('dragstart', handleDragStart, false);
-     //  item.addEventListener('dragenter', handleDragEnter, false);
-     //  item.addEventListener('dragover', handleDragOver, false);
-     //  item.addEventListener('dragleave', handleDragLeave, false);
-     //  item.addEventListener('drop', handleDrop, false);
-     //  item.addEventListener('dragend', handleDragEnd, false);
-     // });
 
     });
 
