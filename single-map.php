78f7e064f1be35cceacf2d8fba6a647aa74a16c9
Huy Eng Heng <huyeng_heng@yahoo.com>
2015-07-01 17:43:09 +0700

    Styling the table in topic page

---
+++ single-map.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<?php jeo_map(); ?>
+
+<?php 
+/* $get_post_type = jeo_get_mapped_post_types();
+unset($get_post_type['rssmi_feed']);
+unset($get_post_type['rssmi_feed_item']);
+unset($get_post_type['site-update']); */
+$paged = get_query_var('paged') ? get_query_var('paged') : 1;
+query_posts(array(
+	'post_type' => jeo_get_mapped_post_types(),
+	'paged' => $paged,
+	's' => isset($_GET['s']) ? $_GET['s'] : null
+));
+if(have_posts()) :
+	?>
+		<div class="section-title">
+			<div class="container">
+				<div class="twelve columns">
+					<h2><?php _e('Latest articles on', 'jeo'); ?> <?php the_title(); ?></H2>
+				</div>
+			</div>
+		</div>
+		<?php get_template_part('loop'); ?>
+<?php
+endif;
+wp_reset_query();
+?>
+
+<?php get_footer(); ?>
