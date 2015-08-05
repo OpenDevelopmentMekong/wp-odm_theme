2c4ffbae0345edbe362e55861d98c4b8ab3730d7
Huy Eng Heng <huyeng_heng@yahoo.com>
2015-08-05 17:30:48 +0700

    Enhance the related recent news

--- inc/od-related-recent-news-widget.php
+++ inc/od-related-recent-news-widget.php
@@ -10,40 +10,39 @@ class OpenDev_Related_Recent_News_Widget extends WP_Widget {
 			__( 'OD Related Recent News Widget', 'opendev' ), // Name
 			array( 'description' => __( 'Display OD the related recent news for the topics', 'opendev' ), ) // Args
 		);
-	} 
+	}
 
 	/**
-	 * Outputs HTML containing a string of the post title as a link 
-	 * 
+	 * Outputs HTML containing a string of the post title as a link
+	 *
 	 * @param category $category a category object to display
 	 */
 
-	public function get_related_news( $category = "") {	  
+	public function get_related_news( $category = "") {
         $args=array(
               'post_type' => 'post',
               'post_status' => 'publish',
               'category_name' => $category,
               'showposts' => 10
-              ); 
-              
+              );
         $news_query = new WP_Query($args);
         if( $news_query->have_posts() ) {
             $news = "<ul>";
-              while ($news_query->have_posts()) : $news_query->the_post();       
+              while ($news_query->have_posts()) : $news_query->the_post();
                 $news .= "<li>";
                  /* if(has_post_thumbnail()) :
 				    $news .= '<a href="'.get_the_permalink().'" title="'.get_the_title().'">';
                          $news .= get_the_post_thumbnail(get_the_ID(), array(50,50), array('class' => 'align-left'));
                     $news .="</a>";
 			     endif; */
-                 $news .= '<a href="'.get_the_permalink().'" rel="bookmark" title="Permanent Link to '.get_the_title().'">'.get_the_title().'</a></li>'; 
+                 $news .= '<a href="'.get_the_permalink().'" rel="bookmark" title="Permanent Link to '.get_the_title().'">'.get_the_title().'</a></li>';
               endwhile;
              $news .= '</ul>';
         }
         wp_reset_query();
         return $news;
-     }        	
-	 
+     }
+
 	/**
 	 * Front-end display of widget.
 	 *
@@ -58,25 +57,28 @@ class OpenDev_Related_Recent_News_Widget extends WP_Widget {
     			if ($news_option == 'Related To The Topics'){
                     $post_title = get_the_title();
             		$category_slug = strtolower(preg_replace('/\s+/', '-', $post_title));
+                }else if ($news_option == 'Show By specific category slug'){
+                    if (!empty ($instance['od_related_news_by_cat_slug']))
+                        $category_slug = $instance['od_related_news_by_cat_slug'];
                 }else{
-                    $category_slug = "";  
+                    $category_slug = "";
                 }
                 $news_exist =  $this->get_related_news( $category_slug );
-    		} 
-    		
-    	if ( $news_exist){    
+    		}
+
+    	if ( $news_exist){
     	   echo $args['before_widget'];
-    	   
+
            if ( ! empty( $instance['od_related_news_title'] ) ) {
     			echo $args['before_title'] . apply_filters( 'widget_title', $instance['od_related_news_title'] ). $args['after_title'];
     		}
-    		
-    		echo "<div>"; 
-    		  echo $this->get_related_news( $category_slug );       		
-    		echo "</div>";                                  		
-    		                        
+
+    		echo "<div>";
+    		  echo $this->get_related_news( $category_slug );
+    		echo "</div>";
+
     	   echo $args['after_widget'];
-    	} //if news avaialable    
+    	} //if news avaialable
 	}
 
 	/**
@@ -87,25 +89,65 @@ class OpenDev_Related_Recent_News_Widget extends WP_Widget {
 	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
-		// outputs the options form on 
-		$title = ! empty( $instance['od_related_news_title'] ) ? $instance['od_related_news_title'] : __( 'Related News', 'opendev' );                                  
-        $news_option = $instance['od_related_news_option'];    
+		// outputs the options form on
+		$title = ! empty( $instance['od_related_news_title'] ) ? $instance['od_related_news_title'] : __( 'Related News', 'opendev' );
+		$by_cat_slug = ! empty( $instance['od_related_news_by_cat_slug'] ) ? $instance['od_related_news_by_cat_slug'] : '';
+        $news_option = $instance['od_related_news_option'];
 		?>
+		 <script type="text/javascript">
+           jQuery(function($) {
+             $('.by_cat_slug').hide();
+             var get_select_id ="";
+             $('.od_related_news_option').change(function(){
+                get_select_id = $(this).attr("id");
+                      if($(this).val() == "Show By specific category slug"){
+                         $('.'+get_select_id+'_by_cat_slug').show();
+                         $('.show_by_cat_slug').show();
+                     }else {
+                         $('.'+get_select_id+'_by_cat_slug').hide();
+                         $('.show_by_cat_slug').hide();
+                     }
+             });
+            /*  alert(get_select_id);
+             if(get_select_id !=""){
+                $('.od_related_news_option').change(function(){
+                    get_select_id = $(this).attr("id");
+                      if($(this).val() == "Show By specific category slug"){
+                         $('.'+get_select_id+'_by_cat_slug').show();
+                     }else {
+                         $('.by_cat_slug').hide();
+                     }
+                 });
+             } */
+
+           });
+         </script>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'od_related_news_title' ); ?>"><?php _e( 'Title:' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'od_related_news_title' ); ?>" name="<?php echo $this->get_field_name( 'od_related_news_title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+			<input class="widefat" id="<?php echo $this->get_field_id( 'od_related_news_title' ); ?>" name="<?php echo $this->get_field_name( 'od_related_news_title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'od_related_news_option' ); ?>"><?php _e( 'Option:' ); ?></label>
-			<select class='widefat' id="<?php echo $this->get_field_id('od_related_news_option'); ?>"
+			<select class='widefat od_related_news_option' id="<?php echo $this->get_field_id('od_related_news_option'); ?>"
                 name="<?php echo $this->get_field_name('od_related_news_option'); ?>" type="text">
-                  <option value='Related To The Topics'<?php echo ($news_option=='Related To The Topics')?'selected':''; ?>>   
+                  <option value='Related To The Topics'<?php echo ($news_option=='Related To The Topics')?'selected':''; ?>>
                   Related To The Topics
-                  </option>  
+                  </option>
                   <option value='The Latest News'<?php echo ($news_option=='The Latest News')?'selected':''; ?>>
                   The Latest News
-                  </option> 
-            </select>      
+                  </option>
+                  <option value='Show By specific category slug'<?php echo ($news_option=='Show By specific category slug')?'selected':''; ?>>
+                  Show by specific category slug
+                  </option>
+            </select>
+       </p>
+       <p class='<?php if($news_option != "Show By specific category slug"){
+                    echo 'by_cat_slug '.$this->get_field_id( 'od_related_news_option_by_cat_slug' );
+             } else echo "show_by_cat_slug"; ?>'>
+			<label for="<?php echo $this->get_field_id( 'od_related_news_by_cat_slug' ); ?>"><?php _e( 'Category slugs:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'od_related_news_by_cat_slug' ); ?>" name="<?php echo $this->get_field_name( 'od_related_news_by_cat_slug' ); ?>" type="text" value="<?php echo esc_attr( $by_cat_slug ); ?>" />  <br />
+            <i>Split the slug by "+". eg. agriculture-and-fishing,sugarcane-bagasse</i>
+
 		</p>
 		<?php
 	}
@@ -124,8 +166,9 @@ class OpenDev_Related_Recent_News_Widget extends WP_Widget {
 		// processes widget options to be saved
 		$instance = array();
 		$instance['od_related_news_title'] = ( ! empty( $new_instance['od_related_news_title'] ) ) ? strip_tags( $new_instance['od_related_news_title'] ) : '';
-	 
-		$instance['od_related_news_option'] = $new_instance['od_related_news_option'] ; 
+
+		$instance['od_related_news_option'] = $new_instance['od_related_news_option'] ;
+		$instance['od_related_news_by_cat_slug'] = $new_instance['od_related_news_by_cat_slug'] ;
 
 		return $instance;
 	}
