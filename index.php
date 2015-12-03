e4a43b7b3fde63703af38b18f2e3add8d00925d0
Christopher ÆVIATOR <christopher@aeviator.cc>
2015-12-03 17:37:31 +0000

    initial header and main menu styling

--- index.php
+++ index.php
@@ -1,90 +1,151 @@
 <?php get_header(); ?>
+<div id="mainNav" class="mainNavOdc">
+      <div class="mainNav-logo">
+        <a href="index.html" target="_self">
+          <span class="icon-od-logo"></span>
 
+        </a>
+      </div>
+
+      <div class="mainNav-inner">
+        <ul id="mainNavElement" class="level1 clearfix">
+          <li class="first jtop act"><a href="index.html" target="_self" id="uid-2">MEKONG</a></li>
+
+          <li class="second"><a href="odc.html" id="uid-3">CAMBODIA</a></li>
+
+          <li class="third"><a href="odl.html" target="_self" id="uid-4">LAOS</a></li>
+
+          <li class="fourth"><a href="odmy.html" target="_self" id="uid-42">MYANMAR</a></li>
+
+          <li class="last jbottom"><a href="odt.html" target="_self" id="uid-5">THAILAND</a></li>
+
+          <li class="last jbottom"><a href="odv.html" target="_self" id="uid-5142">VIETNAM</a></li>
+
+        </ul>
+
+      </div>
+      <!-- <div id="mainNav-social">
+        <a href="#" target="_blank">
+          <span class="sicons twitter"></span>
+        </a>
+        <a href="#" target="_blank">
+          <span class="sicons facebook"></span>
+        </a>
+        <a href="http://www.flickr.com/photos/" target="_blank">
+          <span class="sicons google"></span>
+        </a>
+        <a href="http://vimeo.com/odm/videos" target="_blank">
+          <span class="sicons vimeo"></span>
+        </a>
+      </div> -->
+    </div>
 <?php
-if(is_front_page()) {
- ?>
+if (is_front_page()) {
+    ?>
 
  <?php
   $options = get_option('opendev_options');
-  if($options['site_intro']) :
+    if ($options['site_intro']) :
    $intro_texts = opendev_get_intro_texts();
-   if(!empty($intro_texts)) :
-   ?>                                                 
-   <section id="site-intro" <?php $options_msg = get_option('opendev_options'); if(!empty($options_msg['notice_message']) ): echo "style='margin-top:1em'"; endif; ?>>
+    if (!empty($intro_texts)) :
+   ?>
+   <section id="site-intro" <?php $options_msg = get_option('opendev_options');
+    if (!empty($options_msg['notice_message'])): echo "style='margin-top:1em'";
+    endif;
+    ?>>
     <div class="container">
      <div id="intro-texts" class="row">
-      <?php if($intro_texts[1]) : ?>
+      <?php if ($intro_texts[1]) : ?>
        <div class="four columns">
         <div class="text-item">
          <div class="icon">
-          <?php if($intro_texts[1]['icon']) : ?>
-           <p class="icon-<?php echo $intro_texts[1]['icon']; ?>"></p>
-          <?php endif; ?>
+          <?php if ($intro_texts[1]['icon']) : ?>
+           <p class="icon-<?php echo $intro_texts[1]['icon'];
+    ?>"></p>
+          <?php endif;
+    ?>
          </div>
          <div class="content">
-          <h3><?php echo $intro_texts[1]['title']; ?></h3>
-          <p><?php echo $intro_texts[1]['content']; ?></p>
+          <h3><?php echo $intro_texts[1]['title'];
+    ?></h3>
+          <p><?php echo $intro_texts[1]['content'];
+    ?></p>
          </div>
         </div>
        </div>
-      <?php endif; ?>
-      <?php if($intro_texts[2]) : ?>
+      <?php endif;
+    ?>
+      <?php if ($intro_texts[2]) : ?>
        <div class="four columns">
         <div class="text-item">
          <div class="icon">
-          <?php if($intro_texts[2]['icon']) : ?>
-           <p class="icon-<?php echo $intro_texts[2]['icon']; ?>"></p>
-          <?php endif; ?>
+          <?php if ($intro_texts[2]['icon']) : ?>
+           <p class="icon-<?php echo $intro_texts[2]['icon'];
+    ?>"></p>
+          <?php endif;
+    ?>
          </div>
          <div class="content">
-          <h3><?php echo $intro_texts[2]['title']; ?></h3>
-          <p><?php echo $intro_texts[2]['content']; ?></p>
+          <h3><?php echo $intro_texts[2]['title'];
+    ?></h3>
+          <p><?php echo $intro_texts[2]['content'];
+    ?></p>
          </div>
         </div>
        </div>
-      <?php endif; ?>
-      <?php if($intro_texts[3]) : ?>
+      <?php endif;
+    ?>
+      <?php if ($intro_texts[3]) : ?>
        <div class="four columns">
         <div class="text-item">
          <div class="icon">
-          <?php if($intro_texts[3]['icon']) : ?>
-           <p class="icon-<?php echo $intro_texts[3]['icon']; ?>"></p>
-          <?php endif; ?>
+          <?php if ($intro_texts[3]['icon']) : ?>
+           <p class="icon-<?php echo $intro_texts[3]['icon'];
+    ?>"></p>
+          <?php endif;
+    ?>
          </div>
          <div class="content">
-          <h3><?php echo $intro_texts[3]['title']; ?></h3>
-          <p><?php echo $intro_texts[3]['content']; ?></p>
+          <h3><?php echo $intro_texts[3]['title'];
+    ?></h3>
+          <p><?php echo $intro_texts[3]['content'];
+    ?></p>
          </div>
         </div>
        </div>
-      <?php endif; ?>
+      <?php endif;
+    ?>
      </div>
     </div>
    </section>
-  <?php endif; ?>
+  <?php endif;
+    ?>
  <?php else: ?>
   <!--blank row-->
   <section class="page-section row">
   </section>
- <?php endif; ?>
+ <?php endif;
+    ?>
 
- <?php if($options['frontpage_slider_id']) : ?>
+ <?php if ($options['frontpage_slider_id']) : ?>
    <section id="featured-content" class="page-section row">
     <div class="container">
      <div class="twelve columns">
       <div class="section-featured-content">
         <?php
-          if( function_exists('fa_display_slider') ){
-              fa_display_slider( $options['frontpage_slider_id'] );
+          if (function_exists('fa_display_slider')) {
+              fa_display_slider($options['frontpage_slider_id']);
           }
-        ?>
+    ?>
       </div>
      </div>
     </div>
    </section>
- <?php endif; ?>
+ <?php endif;
+    ?>
 
- <?php $is_mapgroup = jeo_get_mapgroup_data(); ?>
+ <?php $is_mapgroup = jeo_get_mapgroup_data();
+    ?>
  <section id="news" class="page-section row" <?php // if($is_mapgroup) : echo "style='padding-top:60px'"; endif; ?>>
   <div class="container">
    <div class="twelve columns">
@@ -113,34 +174,34 @@ if(is_front_page()) {
             <nav id="tabbed-post-type-nav">
             	<ul>
             		<?php
-            	    $options_news_tags = get_option('opendev_options');
-                    if ($options_news_tags['news_tags'] ){
-            	    $news_tags = preg_replace('/,$/', '', $options_news_tags['news_tags']);
-            	    //$news_tags = rtrim($options_news_tags['news_tags'], ',');
-            	    $news_tags = explode(",", $news_tags);              	    
-            	        $tag_count = 1;
-            		    foreach($news_tags as $tag){
-            		       if($tag_count <= 6){
-                              echo '<li class="tab-tag" id="tag-' .strtolower(str_replace(" ", "-", trim($tag))).'"><div class="tag-name">'.ucwords($tag).'</div></li>';
-                           }
-                           $tag_count++;
-                        }
-                    }else {
-                        echo '<li class="tab-tag" id="tag-regional"><div class="tag-name">Regional</div></li>';
-                        echo '<li class="tab-tag" id="tag-cambodia"><div class="tag-name">Cambodia</div></li>';
-                        echo '<li class="tab-tag" id="tag-laos"><div class="tag-name">Laos</div></li>';
-                        echo '<li class="tab-tag" id="tag-myanmar"><div class="tag-name">Myanmar</div></li>';
-                        echo '<li class="tab-tag" id="tag-thailand"><div class="tag-name">Thailand</div></li>';
-                        echo '<li class="tab-tag" id="tag-vietnam"><div class="tag-name">Vietnam</div></li>';
-                    }
-            	    ?>
+                    $options_news_tags = get_option('opendev_options');
+    if ($options_news_tags['news_tags']) {
+        $news_tags = preg_replace('/,$/', '', $options_news_tags['news_tags']);
+                    //$news_tags = rtrim($options_news_tags['news_tags'], ',');
+                    $news_tags = explode(',', $news_tags);
+        $tag_count = 1;
+        foreach ($news_tags as $tag) {
+            if ($tag_count <= 6) {
+                echo '<li class="tab-tag" id="tag-'.strtolower(str_replace(' ', '-', trim($tag))).'"><div class="tag-name">'.ucwords($tag).'</div></li>';
+            }
+            ++$tag_count;
+        }
+    } else {
+        echo '<li class="tab-tag" id="tag-regional"><div class="tag-name">Regional</div></li>';
+        echo '<li class="tab-tag" id="tag-cambodia"><div class="tag-name">Cambodia</div></li>';
+        echo '<li class="tab-tag" id="tag-laos"><div class="tag-name">Laos</div></li>';
+        echo '<li class="tab-tag" id="tag-myanmar"><div class="tag-name">Myanmar</div></li>';
+        echo '<li class="tab-tag" id="tag-thailand"><div class="tag-name">Thailand</div></li>';
+        echo '<li class="tab-tag" id="tag-vietnam"><div class="tag-name">Vietnam</div></li>';
+    }
+    ?>
             	</ul>
             </nav>
         </section>
     <div class="section-map">
      <?php
      jeo_map();
-     ?>
+    ?>
     </div>
    </div>
   </div>
@@ -150,13 +211,16 @@ if(is_front_page()) {
   <div class="section-title">
    <div class="container">
     <div class="twelve columns">
-     <h2>//<?php _e('Latest news', 'opendev'); ?></h2>
-     //<?php get_template_part('section', 'query-actions'); ?>
+     <h2>//<?php _e('Latest news', 'opendev');
+    ?></h2>
+     //<?php get_template_part('section', 'query-actions');
+    ?>
     </div>
    </div>
   </div>
   <div class="container">
-   //<?php get_template_part('loop', 'latest'); ?>
+   //<?php get_template_part('loop', 'latest');
+    ?>
   </div>
  </section> -->
 
@@ -164,10 +228,12 @@ if(is_front_page()) {
    <div class="container">
      <div class="row">
       <div class="eight columns">
-        <?php dynamic_sidebar('frontpage-footer-left'); ?>
+        <?php dynamic_sidebar('frontpage-footer-left');
+    ?>
       </div>
       <div class="four columns">
-        <?php dynamic_sidebar('frontpage-footer-right'); ?>
+        <?php dynamic_sidebar('frontpage-footer-right');
+    ?>
       </div>
     </div>
    </div>
@@ -175,7 +241,8 @@ if(is_front_page()) {
 
  <?php //get_template_part('section', 'content-summary'); ?>
 
-<?php } ?>
+<?php
+} ?>
 
 <?php // get_template_part('content', 'interactive-map'); ?>
 
