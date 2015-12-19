d6959207572f8a34651a7490d54cdcc9bb31a2dc
chris-aeviator <christopher@aeviator.cc>
2015-12-19 21:00:17 +0100

    fixed semicolon errors

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 ?>
 <?php ?>
 <!DOCTYPE html>
-<html data-country="<?php echo $country ?>" <?php language_attributes(); ?>>
+<html data-country="<?php echo $country; ?>" <?php language_attributes(); ?>>
 
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -131,7 +131,7 @@
         		<div class="container">
         			<div class="three columns align-right">
         				<input type="text" placeholder="<?php _e('Search &#128270;', 'opendev');?>" onfocus="this.placeholder=''" onblur="this.placeholder='<?php _e('Search &#128270;', 'opendev');?>'" />
-        				<img src="<?php bloginfo('stylesheet_directory')?>/img/loading.gif" alt="loading" id="loading" />
+        				<img src="<?php bloginfo('stylesheet_directory');?>/img/loading.gif" alt="loading" id="loading" />
         		    </div>
                     <div class="results-container"></div>
         		</div>
@@ -254,17 +254,17 @@
 
 		      <div class="mainNav-inner">
 		        <ul id="mainNavElement" class="level1 clearfix">
-		          <li class="first jtop <?php if ($country=='mekong') echo 'act'; ?>"><a class="toCkan" data-country="mekong" href="http://pp.<?php echo $domain?>" target="_self" id="uid-2">MEKONG</a></li>
+		          <li class="first jtop <?php if ($country=='mekong') echo 'act'; ?>"><a class="toCkan" data-country="mekong" href="http://pp.<?php echo $domain;?>" target="_self" id="uid-2">MEKONG</a></li>
 
-		          <li class="second <?php if ($country=='cambodia') echo 'act'; ?>"><a class="toCkan" data-country="cambodia" href="http://pp-cambodia.<?php echo $domain?>" id="uid-3">CAMBODIA</a></li>
+		          <li class="second <?php if ($country=='cambodia') echo 'act'; ?>"><a class="toCkan" data-country="cambodia" href="http://pp-cambodia.<?php echo $domain;?>" id="uid-3">CAMBODIA</a></li>
 
-		          <li class="third <?php if ($country=='laos') echo 'act'; ?>"><a class="toCkan" data-country="laos" href="http://pp-laos.<?php echo $domain?>" target="_self" id="uid-4">LAOS</a></li>
+		          <li class="third <?php if ($country=='laos') echo 'act'; ?>"><a class="toCkan" data-country="laos" href="http://pp-laos.<?php echo $domain;?>" target="_self" id="uid-4">LAOS</a></li>
 
-		          <li class="fourth <?php if ($country=='myanmar') echo 'act'; ?>"><a class="toCkan" data-country="myanmar" href="http://pp-myanmar.<?php echo $domain?>" target="_self" id="uid-42">MYANMAR</a></li>
+		          <li class="fourth <?php if ($country=='myanmar') echo 'act'; ?>"><a class="toCkan" data-country="myanmar" href="http://pp-myanmar.<?php echo $domain;?>" target="_self" id="uid-42">MYANMAR</a></li>
 
-		          <li class="last jbottom <?php if ($country=='thailand') echo 'act'; ?>"><a class="toCkan" data-country="thailand" href="http://pp-thailand.<?php echo $domain?>" target="_self" id="uid-5">THAILAND</a></li>
+		          <li class="last jbottom <?php if ($country=='thailand') echo 'act'; ?>"><a class="toCkan" data-country="thailand" href="http://pp-thailand.<?php echo $domain;?>" target="_self" id="uid-5">THAILAND</a></li>
 
-		          <li class="last jbottom <?php if ($country=='vietnam') echo 'act'; ?>"><a class="toCkan" data-country="vietnam" href="http://pp-vietnam.<?php echo $domain?>" target="_self" id="uid-5142">VIETNAM</a></li>
+		          <li class="last jbottom <?php if ($country=='vietnam') echo 'act'; ?>"><a class="toCkan" data-country="vietnam" href="http://pp-vietnam.<?php echo $domain;?>" target="_self" id="uid-5142">VIETNAM</a></li>
 
 		        </ul>
 		      </div>
