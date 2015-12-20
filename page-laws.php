372fb7c62e093e72b4ed1ce479ac4677ded30982
chris-aeviator <christopher@aeviator.cc>
2015-12-20 22:54:03 +0100

    initial

---
+++ page-laws.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+<!-- //cookie -->
+<?php
+// $cookie = $_COOKIE('odm_transition_data');
+// $cookie = json_decode(base64_decode($cookie));
+
+?>
+
+<!-- //cookie -->
+<?php if(have_posts()) : the_post(); ?>
+	<section id="content" class="single-post">
+		<header class="single-post-header">
+			<div class="container">
+				<div class="twelve columns">
+					<h1><?php the_title(); ?></h1>
+				</div>
+			</div>
+		</header>
+		<div class="container laws-container">
+			<div class="twelve columns">
+				<?php the_content(); ?>
+				<?php
+				wp_link_pages( array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'jeo' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+				) );
+				?>
+			</div>
+		</div>
+	</section>
+<?php endif; ?>
+
+<?php get_footer(); ?>
