b02eb2de57dbaeb8a8f54317515fe96c324243ab
Alex Corbi <a.corbi@gmail.com>
2016-01-08 18:29:10 +0100

    building counter

--- page-laws.php
+++ page-laws.php
@@ -10,7 +10,11 @@ Template Name: Laws page
 <?php get_header(); ?>
 
 <?php if(have_posts()) : the_post(); ?>
-	<section id="content" class="single-post">
+
+  <?php $laws_sorted = get_law_datasets_sorted_by_document_type(); ?>
+  <?php $total_laws = get_total_number_of_sorted_datasets($laws_sorted) ?>
+
+  <section id="content" class="single-post">
 		<header class="single-post-header">
 			<div class="container">
 				<div class="twelve columns">
@@ -19,18 +23,25 @@ Template Name: Laws page
 			</div>
 		</header>
 		<div class="container laws-container">
+      <div class="four columns">
+        <div class="counter">
+          Showing <p class="number_records_from">1</p> to <p class="number_records_to"> of <?php echo $total_laws ?> entries
+        </div>
+      </div>
+      <div class="four columns">
+        <div class="pagination">
+          show
+          <select id="law_pagination" name="law_pagination">
+            <option value="1" selected>10</option>
+            <option value="2">25</option>
+            <option value="3">50</option>
+            <option value="4">100</option>
+          </select>
+          entries
+        </div>
+      </div>
 			<div class="eight columns">
-				<div class="pagination">
-					show
-					<select id="law_pagination" name="law_pagination">
-					  <option value="10" selected>10</option>
-						<option value="25">25</option>
-					  <option value="50">50</option>
-					  <option value="100">100</option>
-					</select>
-					entries
-				</div>
-				<?php the_content(); ?>
+      	<?php the_content(); ?>
 				<?php
 				wp_link_pages( array(
 					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'jeo' ) . '</span>',
@@ -40,7 +51,6 @@ Template Name: Laws page
 				) );
 				?>
 					<?php
-						$laws_sorted=get_law_datasets_sorted_by_document_type();
 						foreach ($laws_sorted as $key => $law){?>
 							<div class="document_type_header"><?php echo $key?></div>
 								<table class="law_datasets" id="law_datasets_<?php echo $key;?>">
@@ -103,7 +113,6 @@ Template Name: Laws page
 				<!-- dbug -->
 				<?php// d($laws_sorted);?>
 
-
 			</div>
 			<div class="one column">&nbsp;</div>
 			<div class="three columns">
@@ -112,7 +121,7 @@ Template Name: Laws page
 						<span class="big">SEARCH</span> in Laws
 					</div>
 					<div class="law_search_box_wrapper">
-						<input type="text" id="Search_All" placeholder="Search all Laws">
+						<input type="text" id="search_all" placeholder="Search all Laws">
 					</div>
 				</div>
 			</div>
@@ -124,90 +133,80 @@ Template Name: Laws page
 <?php get_footer(); ?>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.js"></script>
 <script type="text/javascript">
+
 jQuery(document).ready(function($) {
 	// $('#law_datasets_anukretsub-decree').dataTable();
+  // multiple tables
+  $.fn.dataTableExt.oApi.fnFilterAll = function (oSettings, sInput, iColumn, bRegex, bSmart) {
+   var settings = $.fn.dataTableSettings;
+   for (var i = 0; i < settings.length; i++) {
+     settings[i].oInstance.fnFilter(sInput, iColumn, bRegex, bSmart);
+   }
+  };
 
-// multiple tables
-$.fn.dataTableExt.oApi.fnFilterAll = function (oSettings, sInput, iColumn, bRegex, bSmart) {
-               var settings = $.fn.dataTableSettings;
+  $(document).ready(function () {
+     $('.law_datasets').dataTable({
+       "bPaginate": true,
+     });
 
-               for (var i = 0; i < settings.length; i++) {
-                   settings[i].oInstance.fnFilter(sInput, iColumn, bRegex, bSmart);
-               }
-           };
+		//  set datatables
+     var oTable0 = $("#law_datasets_anukretsub-decree").dataTable();
+		 var oTable1 = $("#law_datasets_chbablawkram").dataTable();
+		 var oTable2 = $("#law_datasets_constitution-of-cambodia").dataTable();
+		 var oTable3 = $("#law_datasets_international-treatiesagreements").dataTable();
+		 var oTable4 = $("#law_datasets_kech-sonyacontractagreement").dataTable();
+		 var oTable5 = $("#law_datasets_kolkar-nenomguidelines").dataTable();
+		 var oTable6 = $("#law_datasets_kolnyobaypolicy").dataTable();
+		 var oTable7 = $("#law_datasets_likhetletter").dataTable();
+		 var oTable8 = $("#law_datasets_prakasjoint-prakasproclamation").dataTable();
+		 var oTable9 = $("#law_datasets_preah-reach-kramroyal-kram").dataTable();
+		 var oTable10 = $("#law_datasets_sarachorcircular").dataTable();
+		 var oTable11 = $("#law_datasets_sechkdei-chhun-damneoungnoticeannouncement").dataTable();
+		 var oTable12 = $("#law_datasets_sechkdei-nenuminstruction").dataTable();
+		 var oTable13 = $("#law_datasets_sechkdei-preang-chbabdraft-laws-amp-regulations").dataTable();
+		 var oTable14 = $("#law_datasets_sechkdei-samrechdecision").dataTable();
+		 var oTable15 = $("#law_datasets_other").dataTable();
 
-           $(document).ready(function () {
-               $('.law_datasets').dataTable({
-                   "bPaginate": true,
+     $("#search_all").keyup(function () {
+       console.log("filtering page " + this.value);
+			 //  set filters
+       oTable0.fnFilterAll(this.value);
+  		 oTable1.fnFilterAll(this.value);
+  		 oTable2.fnFilterAll(this.value);
+  		 oTable3.fnFilterAll(this.value);
+  		 oTable4.fnFilterAll(this.value);
+  		 oTable5.fnFilterAll(this.value);
+  		 oTable5.fnFilterAll(this.value);
+  		 oTable6.fnFilterAll(this.value);
+  		 oTable7.fnFilterAll(this.value);
+  		 oTable8.fnFilterAll(this.value);
+  		 oTable9.fnFilterAll(this.value);
+  		 oTable10.fnFilterAll(this.value);
+  		 oTable11.fnFilterAll(this.value);
+  		 oTable12.fnFilterAll(this.value);
+  		 oTable13.fnFilterAll(this.value);
+  		 oTable14.fnFilterAll(this.value);
+  		 oTable15.fnFilterAll(this.value);
+    });
+  });
 
-               });
-							//  set datatables
-               var oTable0 = $("#law_datasets_anukretsub-decree").dataTable();
-							 var oTable1 = $("#law_datasets_chbablawkram").dataTable();
-							 var oTable2 = $("#law_datasets_constitution-of-cambodia").dataTable();
-							 var oTable3 = $("#law_datasets_international-treatiesagreements").dataTable();
-							 var oTable4 = $("#law_datasets_kech-sonyacontractagreement").dataTable();
-							 var oTable5 = $("#law_datasets_kolkar-nenomguidelines").dataTable();
-							 var oTable6 = $("#law_datasets_kolnyobaypolicy").dataTable();
-							 var oTable7 = $("#law_datasets_likhetletter").dataTable();
-							 var oTable8 = $("#law_datasets_prakasjoint-prakasproclamation").dataTable();
-							 var oTable9 = $("#law_datasets_preah-reach-kramroyal-kram").dataTable();
-							 var oTable10 = $("#law_datasets_sarachorcircular").dataTable();
-							 var oTable11 = $("#law_datasets_sechkdei-chhun-damneoungnoticeannouncement").dataTable();
-							 var oTable12 = $("#law_datasets_sechkdei-nenuminstruction").dataTable();
-							 var oTable13 = $("#law_datasets_sechkdei-preang-chbabdraft-laws-amp-regulations").dataTable();
-							 var oTable14 = $("#law_datasets_sechkdei-samrechdecision").dataTable();
-							 var oTable15 = $("#law_datasets_other").dataTable();
-
-               $("#Search_All").keyup(function () {
-                   // Filter on the column (the index) of this element
-									//  set filters
-                   oTable0.fnFilterAll(this.value);
-									 oTable1.fnFilterAll(this.value);
-									 oTable2.fnFilterAll(this.value);
-									 oTable3.fnFilterAll(this.value);
-									 oTable4.fnFilterAll(this.value);
-									 oTable5.fnFilterAll(this.value);
-									 oTable5.fnFilterAll(this.value);
-									 oTable6.fnFilterAll(this.value);
-									 oTable7.fnFilterAll(this.value);
-									 oTable8.fnFilterAll(this.value);
-									 oTable9.fnFilterAll(this.value);
-									 oTable10.fnFilterAll(this.value);
-									 oTable11.fnFilterAll(this.value);
-									 oTable12.fnFilterAll(this.value);
-									 oTable13.fnFilterAll(this.value);
-									 oTable14.fnFilterAll(this.value);
-									 oTable15.fnFilterAll(this.value);
-
-               });
-           });
-
-          //  $(document).ready(function () {
-          //      $('#law_datasets_other').dataTable({
-          //          "bPaginate": true,
-					 //
-          //      });
-          //      var oTable1 = $("#law_datasets_other").dataTable();
-					 //
-          //      $("#Search_All").keyup(function () {
-          //          // Filter on the column (the index) of this element
-          //          oTable1.fnFilterAll(this.value);
-          //      });
-          //  });
-
-
-					// detect pagination change
-					// $(document).ready(function () {
-					// 	$('#law_pagination').on('change', function() {
-					// 		$('div.dataTables_length select').val(this.value);
-					// 		// alert( this.value ); // or $(this).val()
-					// 	});
-					//
-					// });
-
-
-
+  //  $(document).ready(function () {
+  //    $('#law_datasets_other').dataTable({
+  //        "bPaginate": true,
+  //    });
+  //    var oTable1 = $("#law_datasets_other").dataTable();
+  //
+  //    $("#search_all").keyup(function () {
+  //        oTable1.fnFilterAll(this.value);
+  //    });
+  //  });
+  //
+	$(document).ready(function () {
+		$('#law_pagination').on('change', function() {
+      console.log("changing page " + this.value);
+			$('div.dataTables_length select').val(this.value);
+		});
+	});
 
 
 });
