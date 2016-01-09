b47d83d1f81e1e44ae7dbc8d0b69e049eea145f0
chris-aeviator <christopher@aeviator.cc>
2016-01-09 20:35:52 +0700

    removed page title and text content

--- page-map-explorer.php
+++ page-map-explorer.php
@@ -7,17 +7,6 @@ Template Name: Map explorer
 
 <?php if(have_posts()) : the_post(); ?>
 
- <section id="page-content">
-  <div class="section-title">
-   <div class="container">
-    <div class="twelve columns">
-     <h1><?php the_title(); ?></h1>
-     <?php the_content(); ?>
-    </div>
-   </div>
-  </div>
- </section>
-
  <section id="map">
   <div class="section-title">
    <div class="container">
