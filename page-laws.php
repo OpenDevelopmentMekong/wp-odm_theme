79883ec7658ca7794284c905db5bd9d16d0f978a
Alex Corbi <a.corbi@gmail.com>
2016-01-10 01:53:04 +0100

    counter is handled by datatables

--- page-laws.php
+++ page-laws.php
@@ -96,14 +96,12 @@ jQuery(document).ready(function($) {
 
   console.log("laws pages init");
 
-  var total_laws = $('.records_total').text();
-
   $.fn.dataTableExt.oApi.fnFilterAll = function (oSettings, sInput, iColumn, bRegex, bSmart) {
    var settings = $.fn.dataTableSettings;
    for (var i = 0; i < settings.length; i++) {
      settings[i].oInstance.fnFilter(sInput, iColumn, bRegex, bSmart);
    }
- };
+  };
 
   var oTable = $("#law_datasets").dataTable();
 
