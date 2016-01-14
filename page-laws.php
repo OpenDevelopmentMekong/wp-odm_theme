846be776ea8ee1e0eadc600854f2c05ea4952a39
Alex Corbi <a.corbi@gmail.com>
2016-01-14 23:48:50 +0100

    fixes #432

--- page-laws.php
+++ page-laws.php
@@ -43,6 +43,9 @@ Template Name: Laws page
           </thead>
           <tbody
             <?php foreach ($laws["wpckan_dataset_list"] as $law_record): ?>
+              <?php if (is_null($law_record['wpckan_dataset_extras']['wpckan_dataset_extras-odm_document_type'])){
+                continue;
+              }?>
               <tr>
                 <td class="law_odm_document_type">
                   <?php echo $law_record['wpckan_dataset_extras']['wpckan_dataset_extras-odm_document_type'];?>
