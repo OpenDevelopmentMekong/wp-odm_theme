69a0a3a22e00b1540e072cee37555e4486a9f297
chris-aeviator <christopher@aeviator.cc>
2016-02-15 10:44:26 +0100

    prevent legend toggle from firing multiple times fixes#570

--- inc/interactive-map.php
+++ inc/interactive-map.php
@@ -337,16 +337,17 @@ class OpenDev_InteractiveMap {
                  $(this).parent().find('.toggle-text').show();
                  $(this).parent().find('.toggles').css('display','block');
 
-                 $layers_active.find('.layer-item .toggles .toggle-legend').on('click', function() {
+                 $layers_active.find('.layer-item .toggles .toggle-legend').off('click').on('click', function() {
                    if($(this).html() == "Show legend"){
-                    //$(this).addClass('active');
-                    $(this).parent().parent().find('.legend').show();
-                    $(this).html('Hide legend');
-                   } else {
-                    //$(this).removeClass('active');
-                    $(this).parent().parent().find('.legend').hide();
-                    $(this).html('Show legend');
-                   }
+                  //$(this).addClass('active');
+                  $(this).parent().parent().find('.legend').show();
+                  $(this).html('Hide legend');
+                 } else {
+                  //$(this).removeClass('active');
+                  $(this).parent().parent().find('.legend').hide();
+                  $(this).html('Show legend');
+                 }
+
                  });
 
 
@@ -361,15 +362,6 @@ class OpenDev_InteractiveMap {
                }
            });
           });
-
-
-      // $layers.find('.layer-item .toggle-info').on('click', function() {
-      //     $(this).toggleClass('active');
-      //     $(this).closest('.layer-excerpt').show();
-      //
-      // });
-
-
 
       $layers.find('.layer-item .toggles .toggle-text').on('click', function() {
        if($(this).html() == "More"){
@@ -407,17 +399,6 @@ class OpenDev_InteractiveMap {
 
 
      });
-
-     /*var $category_toggles = $('.map .layer-toggle')
-     $category_toggles.find('a').on('click', function() {
-      if($(this).hasClass('active')){
-       disableAllLayers();
-       $(this).removeClass('active');
-      } else {
-       enableAllLayers();
-       $(this).addClass('active');
-      }
-    });*/
 
      if (ENABLE_DND){
        [].forEach.call(category_items, function(item) {
