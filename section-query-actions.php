6989a0e4517f317a941f4e8635207f07bf8738ba
huyeng <huyeng_heng@yahoo.com>
2016-04-08 11:09:05 +0700

    Fixes issue logged

--- section-query-actions.php
+++ section-query-actions.php
@@ -8,18 +8,19 @@
 	$geojson = jeo_get_api_url($args);
 	$download = jeo_get_api_download_url($args);
 	$rss = add_query_arg(array('feed' => 'rss'));
+	$rss = str_replace("news-archive/", "", $rss);
 	?>
 	<span class="query-actions-title"><?php _e('Content API', 'opendev'); ?></span>
-	<a class="rss" href="<?php echo $rss; ?>">
+	<a class="rss" target="_blank" href="<?php echo $rss; ?>">
 		<span class="icon-rss"></span>
 		<?php _e('RSS Feed', 'infoamazonia'); ?>
 	</a>
 	<?php if($use_api) : ?>
-		<a class="geojson" href="<?php echo $geojson; ?>">
+		<a class="geojson" target="_blank" href="<?php echo $geojson; ?>">
 			<span class="icon-hair-cross"></span>
 			<?php _e('Get GeoJSON', 'infoamazonia'); ?>
 		</a>
-		<a class="download" href="<?php echo $download; ?>">
+		<a class="download" target="_blank" href="<?php echo $download; ?>">
 			<span class="icon-download"></span>
 			<?php _e('Download', 'infoamazonia'); ?>
 		</a>
--- single-topic.php
+++ single-topic.php
@@ -12,7 +12,7 @@
     					<div class="categories">
     						  <span class="lsf">&#9776;</span> <?php echo __( 'Filed under:', 'jeo' ); ?> <?php the_category(); ?>
     					</div>
-            		</header>
+				    </header>
             		<?php
             		if(jeo_has_marker_location()) {
             			?>
@@ -28,69 +28,69 @@
             			<?php
             		}
             		?>
-            		<section class="content">
+				    <section class="content">
     					<div class="post-content">
     						<?php the_content(); ?>
     					</div>
-    					<?php
-    					wp_link_pages( array(
-    						'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'jeo' ) . '</span>',
-    						'after'       => '</div>',
-    						'link_before' => '<span>',
-    						'link_after'  => '</span>',
-    					) );
-    					?>
-    					<?php comments_template(); ?>
-        				</div>
-    	       	   </section>
-    				<div class="three columns offset-by-one">
-    					<aside id="sidebar">
-    						<ul class="widgets">
-    							<li class="widget share-widget">
-    								<div class="share clearfix">
-    									<ul>
-    										<li>
-    											<div class="fb-share-button" data-href="<?php echo get_permalink( $post->ID )?>" data-send="false" data-layout="button" data-show-faces="false"></div>
-    										</li>
-    										<li>
-    											<div class="twitter-share-button"><a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-lang="en" data-count="vertical">Tweet</a></div>
-    										</li>
-    										<li>
-    											<div class="g-plusone" data-width="50" data-annotation="none" data-size="tall" data-href="<?php the_permalink(); ?>" data-count="false"></div>
-    										</li> 
-    									</ul>
-    								</div>
-    							</li>
-    							<li class="widget">
-    								<?php opendev_summary(); ?>
-    							</li>
-    							<?php dynamic_sidebar('topic'); ?>
-    							<?php if (get_group('related_link') != "" && get_group('related_link') != NULL) { ?>
-                            	<li class="widget widget_opendev_related_link_widget" style="clear:left">
-                                    <div>
-                                    <h2 class="widget-title">Related Links</h2>
-    							<?php
-                                        $related_link  = get_group('related_link');
-                                        echo '<ul>';
-                                        foreach ($related_link as $related) {
-                                            echo '<li>';
-                                            if($related['related_link_link'][1]!="")
-    											echo '<a title="Click to view." href="'.$related['related_link_link'][1].'">';
-    										if($related['related_link_label'][1]!="")
-    										 	echo $related['related_link_label'][1];
-    										if($related['related_link_link'][1]!="")
-                                            	echo '</a>';
-                                            echo '</li>';
-                                        }
-                                        echo '</ul>';
-                                    ?>
-                                    </div>
-    							</li>
-    						<?php } ?>
-    						</ul>
-    					</aside>
-    				</div>
-    			</div>
+		    					<?php
+		    					wp_link_pages( array(
+		    						'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'jeo' ) . '</span>',
+		    						'after'       => '</div>',
+		    						'link_before' => '<span>',
+		    						'link_after'  => '</span>',
+		    					) );
+		    					?>
+		    					<?php comments_template(); ?>
+					    </section>
+				</div>
+				<div class="three columns offset-by-one">
+					<aside id="sidebar">
+						<ul class="widgets">
+							<li class="widget share-widget">
+								<div class="share clearfix">
+									<ul>
+										<li>
+											<div class="fb-share-button" data-href="<?php echo get_permalink( $post->ID )?>" data-send="false" data-layout="button" data-show-faces="false"></div>
+										</li>
+										<li>
+											<div class="twitter-share-button"><a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-lang="en" data-count="vertical">Tweet</a></div>
+										</li>
+										<li>
+											<div class="g-plusone" data-width="50" data-annotation="none" data-size="tall" data-href="<?php the_permalink(); ?>" data-count="false"></div>
+										</li>
+									</ul>
+								</div>
+							</li>
+							<li class="widget">
+								<?php opendev_summary(); ?>
+							</li>
+							<?php dynamic_sidebar('topic'); ?>
+							<?php if (get_group('related_link') != "" && get_group('related_link') != NULL) { ?>
+                        	<li class="widget widget_opendev_related_link_widget" style="clear:left">
+                                <div>
+                                <h2 class="widget-title">Related Links</h2>
+							<?php
+                                    $related_link  = get_group('related_link');
+                                    echo '<ul>';
+                                    foreach ($related_link as $related) {
+                                        echo '<li>';
+                                        if($related['related_link_link'][1]!="")
+											echo '<a title="Click to view." href="'.$related['related_link_link'][1].'">';
+										if($related['related_link_label'][1]!="")
+										 	echo $related['related_link_label'][1];
+										if($related['related_link_link'][1]!="")
+                                        	echo '</a>';
+                                        echo '</li>';
+                                    }
+                                    echo '</ul>';
+                                ?>
+                                </div>
+							</li>
+						<?php } ?>
+						</ul>
+					</aside>
+				</div>
+			</div>
 	</article>
 
 <?php endif; ?>
