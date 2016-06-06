7e8e391f72fdfb377ec0f440778001980435a5d7
huyeng <huyeng_heng@yahoo.com>
2016-06-06 15:46:46 +0700

    Updates get_metadata_info_of_dataset_by_id to hide table if no dataset value

--- functions.php
+++ functions.php
@@ -1291,36 +1291,111 @@ function print_category_by_post_type( $category, $post_type ="post", $current_ca
   }else {
      $current_page = "";
   }
-  echo "<span class='nochildimage-".COUNTRY_NAME.$current_page."'>";
-          echo '<a href="' . get_category_link( $category->cat_ID ) . '?post_type='.$post_type.'">';
-              if ($current_cat == $category->slug){ // if page of the topic exists
-                  echo "<strong class='".COUNTRY_NAME."-color'>";
-                      echo $category->name;
-                  echo "</strong>";
-              }else{
-                    echo $category->name;
-              }
-          echo "</a>";
-    echo "</span>";
+  if($category->cat_ID){
+    $cat_ID = $category->cat_ID;
+    $get_category_link = get_category_link( $category->cat_ID );
+  }else if($category->term_id){
+    $cat_ID = $category->term_id;
+    $get_category_link = get_term_link( $category);
+  }
+  if($post_type == "map-layer"){
+    echo '<a href="' . $get_category_link. '?post_type='.$post_type.'">';
+          echo $category->name;
+    echo "</a>";
+    $args_get_post = array(
+        'post_type' => $post_type,
+        'tax_query' => array(
+                            array(
+                              'taxonomy' => $category->taxonomy,
+                              'field' => 'id',
+                              'terms' => $cat_ID, // Where term_id of Term 1 is "1".
+                              'include_children' => false
+                            )
+                          )
+    );
+    $query_get_post = new WP_Query( $args_get_post );
+    if($query_get_post->have_posts() ){
+      echo "<ul class='cat-layers switch-layers'>";
+      while ( $query_get_post->have_posts() ) : $query_get_post->the_post(); ?>
+        <li class="layer-item" data-layer="<?php the_ID(); ?>" id="post-<?php the_ID(); ?>">
+            <span class="list-circle-active"></span>
+            <span class="list-circle-o"></span>
+            <span class="layer-item-name"><?php the_title(); ?></span>
+            <?php
+            if ( (CURRENT_LANGUAGE != "en") ){
+              $layer_legend = get_post_meta(get_the_ID(), '_layer_legend_localization', true);
+              $layer_download_link = get_post_meta(get_the_ID(), '_layer_download_link_localization', true);
+              $layer_profilepage_link = get_post_meta(get_the_ID(), '_layer_profilepage_link_localization', true);
+            }else {
+              $layer_legend = get_post_meta(get_the_ID(), '_layer_legend', true);
+              $layer_download_link = get_post_meta(get_the_ID(), '_layer_download_link', true);
+              $layer_profilepage_link = get_post_meta(get_the_ID(), '_layer_profilepage_link', true);
+            }
 
+            if($layer_legend!=""){ ?>
+              <div class="legend"><?php echo $layer_legend; ?></div>
+            <?php }
+            if($layer_download_link!=""){ ?>
+              <a class="download-url" href="<?php echo $layer_download_link; ?>" target="_blank"><i class="fa fa-arrow-down"></i></a>
+            <?php }
+            if($layer_profilepage_link!=""){ ?>
+              <a class="toggle-info" alt="Info" href="#"><i id="<?php echo the_ID(); ?>" class="fa fa-info-circle"></i></a>
+              <a class="profilepage_link" href="<?php echo $layer_profilepage_link; ?>" target="_blank"><i class="fa fa-table"></i></a>
+            <?php } ?>
+        </li>
+      <?php endwhile;
+      echo "</ul>";
+      wp_reset_postdata();
+    } //$query_get_post->have_posts
+  }else {
+    echo "<span class='nochildimage-".COUNTRY_NAME.$current_page."'>";
+            echo '<a href="' . get_category_link( $category->cat_ID ) . '?post_type='.$post_type.'">';
+                if ($current_cat == $category->slug){ // if page of the topic exists
+                    echo "<strong class='".COUNTRY_NAME."-color'>";
+                        echo $category->name;
+                    echo "</strong>";
+                }else{
+                      echo $category->name;
+                }
+            echo "</a>";
+      echo "</span>";
+  }
 }
 function walk_child_category_by_post_type( $children, $post_type, $current_cat = "") {
-  foreach($children as $child){
-    // Get immediate children of current category
-    $cat_children = get_categories( array('parent' => $child->term_id, 'hide_empty' => 1, 'orderby' => 'term_id', ) );
-    echo "<li>";
-    // Display current category
-      print_category_by_post_type($child, $post_type, $current_cat);
-    // if current category has children
-    if ( !empty($cat_children) ) {
-      // add a sublevel
-      echo "<ul>";
-      // display the children
-        walk_child_category_by_post_type( $cat_children, $post_type, $current_cat );
-      echo "</ul>";
+    if($post_type == "map-layer"){
+      foreach($children as $child){
+        $cat_children = get_categories( array('parent' => $child->term_id, 'hide_empty' => 1, 'orderby' => 'name', ) );
+        echo "<li class='cat-item cat-item-".$child->term_id."'>";
+        // Display current category
+          print_category_by_post_type($child, $post_type, $current_cat);
+        // if current category has children
+        if ( !empty($cat_children) ) {
+          // add a sublevel
+          echo "<ul class='cat-layers switch-layers'>";
+          // display the children
+            walk_child_category_by_post_type( $cat_children, $post_type, $current_cat);
+          echo "</ul>";
+        }
+        echo "</li>";
+      }
+    }else { //widget
+        foreach($children as $child){
+          // Get immediate children of current category
+          $cat_children = get_categories( array('parent' => $child->term_id, 'hide_empty' => 1, 'orderby' => 'term_id', ) );
+          echo "<li>";
+          // Display current category
+            print_category_by_post_type($child, $post_type, $current_cat, $widget);
+          // if current category has children
+          if ( !empty($cat_children) ) {
+            // add a sublevel
+            echo "<ul>";
+            // display the children
+              walk_child_category_by_post_type( $cat_children, $post_type, $current_cat,  $widget);
+            echo "</ul>";
+          }
+          echo "</li>";
+        }
     }
-    echo "</li>";
-  }
 }
 
 /** END CATEGORY */
@@ -1624,68 +1699,85 @@ function get_metadata_info_of_dataset_by_id($ckan_domain,$ckan_dataset_id, $indi
                       );
 
   // get ckan record by id
+
   $get_info_from_ckan = get_dataset_by_id($ckan_domain,$ckan_dataset_id);
+  if($ckan_dataset_id!=""){
   ?>
-    <div class="layer-toggle-info toggle-info toggle-info-<?php echo $individual_layer['ID']; ?>">
+    <div class="layer-toggle-info toggle-info toggle-info-<?php echo $individual_layer->ID; ?>">
         <table border="0" class="toggle-talbe">
           <tr><td colspan="2"><h5><?php echo $get_info_from_ckan['title_translated'][$lang] ?></h5></td></tr>
           <?php
           if($showing_fields == ""){
             if($get_info_from_ckan){
               foreach ($get_info_from_ckan as $key => $info) {
-                if($key == 'license_id'){  ?>
-                  <tr>
-                      <td><?php echo $attribute_metadata['license_id']; ?></td>
-                      <td><?php echo $info == "unspecified"? ucwords($get_info_from_ckan['license_id'] ) : $get_info_from_ckan['license_id']; ?></td>
-                  </tr>
-              <?php }else{
-                  if(array_key_exists($key, $attribute_metadata)){
-              ?>    <tr>
-                        <td><?php echo $attribute_metadata[$key]; ?></td><td><?php echo is_array($info) ? $info[$lang]: $info; ?></td>
-                    </tr>
-          <?php   }
-                }
+                if(!empty($get_info_from_ckan)){
+                    if($key == 'license_id' && $get_info_from_ckan['license_id']!=""){ ?>
+                      <tr>
+                          <td><?php echo $attribute_metadata['license_id']; ?></td>
+                          <td><?php echo $info == "unspecified"? ucwords($get_info_from_ckan['license_id'] ) : $get_info_from_ckan['license_id']; ?></td>
+                      </tr>
+                    <?php
+                    }else{
+                        if(array_key_exists($key, $attribute_metadata)){
+                    ?>    <tr>
+                              <td><?php echo $attribute_metadata[$key]; ?></td><td><?php echo is_array($info) ? $info[$lang]: $info; ?></td>
+                          </tr>
+                    <?php
+                        }
+                    }//end else
+                } //!empty($get_info_from_ckan)
               } //end foreach
-            }//if get resource
-          }else { //if so fields are defined
+            }//if get $get_info_from_ckan
+          }else { //if show fields are defined
             foreach ($showing_fields as $key => $info) {
-              if($key == 'license_id'){  ?>
-                <tr>
-                    <td><?php echo $showing_fields['license_id']; ?></td>
-                    <td><?php echo $info == "unspecified"? ucwords($get_info_from_ckan['license_id'] ) : $get_info_from_ckan['license_id']; ?></td>
-                </tr>
-            <?php }else{
-                  if($get_info_from_ckan){
-              ?>    <tr>
-                        <td><?php echo $showing_fields[$key]; ?></td>
-                        <td><?php echo is_array($get_info_from_ckan[$key]) ? $get_info_from_ckan[$key][$lang]: $get_info_from_ckan[$key]; ?></td>
+              if(!empty($get_info_from_ckan)){
+                  if($key == 'license_id' && $get_info_from_ckan['license_id']!=""){ ?>
+                    <tr>
+                        <td><?php echo $showing_fields['license_id']; ?></td>
+                        <td><?php echo $info == "unspecified"? ucwords($get_info_from_ckan['license_id'] ) : $get_info_from_ckan['license_id']; ?></td>
                     </tr>
-        <?php     }
-              }
+                  <?php
+                  }else{  ?>
+                      <tr>
+                          <td><?php echo $showing_fields[$key]; ?></td>
+                          <td><?php echo is_array($get_info_from_ckan[$key]) ? $get_info_from_ckan[$key][$lang]: $get_info_from_ckan[$key]; ?></td>
+                      </tr>
+                  <?php
+                  }
+              } //!empty($get_info_from_ckan)
             } //end foreach
           }
           ?>
         </table>
-      <?php if ($atlernative_links == 1) { ?>
-        <div class="atlernative_links">
-        <?php if ($lang != 'en'){ ?>
-                <div class="div-button"><a href="<?php echo $individual_layer['download_url_localization']; ?>" target="_blank"><i class="fa fa-arrow-down"></i> <?php _e("Download data", "opendev"); ?></a></div>
-
-                <?php if ($individual_layer['profilepage_url_localization']){ ?>
-                  <div class="div-button"><a href="<?php echo $individual_layer['profilepage_url_localization']; ?>" target="_blank"><i class="fa fa-table"></i> <?php _e("View dataset table", "opendev"); ?></a></div>
-                <?php } ?>
-        <?php }else {  ?>
-                <div class="div-button"><a href="<?php echo $individual_layer['download_url']; ?>" target="_blank"><i class="fa fa-arrow-down"></i> <?php _e("Download data", "opendev"); ?></a></div>
-
-                <?php if ($individual_layer['profilepage_url']){ ?>
-                  <div class="div-button"><a href="<?php echo $individual_layer['profilepage_url']; ?>" target="_blank"><i class="fa fa-table"></i> <?php _e("View dataset table", "opendev"); ?></a></div>
-                <?php } ?>
-        <?php } ?>
-        </div><!-- atlernative_links -->
-      <?php } //if $atlernative_links ?>
+      <?php
+      if ($atlernative_links == 1) {
+          $get_post_by_id = get_post($individual_layer->ID);
+          if ( (CURRENT_LANGUAGE != "en") ){
+             $get_download_url = get_post_meta($get_post_by_id->ID, '_layer_download_link_localization', true);
+             $get_profilepage_url = get_post_meta($get_post_by_id->ID, '_layer_profilepage_link_localization', true);
+          }else {
+             $get_download_url = get_post_meta($get_post_by_id->ID, '_layer_download_link', true);
+             $get_profilepage_url = get_post_meta($get_post_by_id->ID, '_layer_profilepage_link', true);
+          }
+          ?>
+          <div class="atlernative_links">
+          <?php if ($get_download_url != ''){ ?>
+                  <div class="div-button"><a href="<?php echo $get_download_url; ?>" target="_blank"><i class="fa fa-arrow-down"></i> <?php _e("Download data", "opendev"); ?></a></div>
+               <?php
+               }
+               if ($get_profilepage_url != ''){ ?>
+                    <div class="div-button"><a href="<?php echo $get_profilepage_url; ?>" target="_blank"><i class="fa fa-table"></i> <?php _e("View dataset table", "opendev"); ?></a></div>
+               <?php
+               } ?>
+          </div><!-- atlernative_links -->
+        <?php
+      } //if $atlernative_links ?>
     </div><!--layer-toggle-info-->
 <?php
+  }//if ckan_dataset_id !=""
+
 }//end function
+
 function buildStyledTopTopicListForLaws($lang)
 {
     $navigation_vocab = @file_get_contents(get_stylesheet_directory().'/odm-taxonomy/top_topics/top_topics_multilingual.json');
