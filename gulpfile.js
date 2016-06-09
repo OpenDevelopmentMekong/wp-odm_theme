1abe45a18d523013cfb39daba6caf9e34d40c728
DragonVirus <limonster.li@gmail.com>
2016-06-09 17:11:09 +0630

    Add gulp.watch task and add template tasks for styles and scripts

--- gulpfile.js
+++ gulpfile.js
@@ -9,25 +9,44 @@ var LESS_PATH = 'less/';
 var JS_PATH = 'js/';
 var CSS_PATH = 'css/';
 var DEST_PATH = 'dist/'; //Temp path will change later
+var CSS_DIST_PATH = DEST_PATH + 'css/';
+var JS_DIST_PATH = DEST_PATH + 'js/';
 
 
 gulp.task('default', function() {
   runSequence(
+  	'build-less',
   	'build-styles'
   );
 });
 
-gulp.task('build-styles', function(){
+gulp.task('build-less', function(){
 	return gulp.src(LESS_PATH + 'odm.less')
 	.pipe(less())
 	.pipe(cleanCSS())
 	.pipe(gulp.dest(DEST_PATH));
 });
 
-gulp.task('build-scripts', function(){
+gulp.task('build-styles', function(){
+	return gulp.src([
 
+	])
+	.pipe(cleanCSS())
+	.pipe(concat('style.min.css'))
+	.pipe(gulp.dest(DEST_PATH));
+});
+
+gulp.task('build-scripts', function(){
+	return gulp.src([
+
+	])
+	.pipe(uglify())
+	.pipe(concat('script.min.js', {newLine: ';'}))
+	.pipe(gulp.dest(DEST_PATH));
 });
 
 gulp.task('watch', function(){
-
+	gulp.watch(LESS_PATH + '/*less', ['build-less', 'build-styles']);
+	gulp.watch(CSS_PATH + '/*.css', ['build-styles']);
+	gulp.watch(JS_PATH + '/*.js', ['build-scripts']);
 });
