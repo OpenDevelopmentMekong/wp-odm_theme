785ed15b50515306eabcb8bb452fc3a2acd85d68
Alex Corbi <a.corbi@gmail.com>
2016-06-21 17:25:05 +0200

    added page template for dataset detail

---
+++ page-dataset-detail.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Template Name: WPCKAN Dataset detail
+ */
+?>
+<?php get_header(); ?>
+
+<?php if(have_posts()) : the_post(); ?>
+	<section id="content" class="single-post">
+
+		<div class="container">
+			<div class="row">
+				<div class="twelve columns">
+					<?php the_content(); ?>
+					<?php
+						if (isset($_GET['id'])):
+							echo do_shortcode('[wpckan_dataset_detail id="' . $_GET['id'] . '"]');
+						else:
+							echo "<p>" . _e('Please provide an id as parameter', 'wpckan') . "</p>";
+						endif;
+					?>
+				</div>
+			</div>
+
+		</div>
+	</section>
+<?php endif; ?>
+
+<?php get_footer(); ?>
