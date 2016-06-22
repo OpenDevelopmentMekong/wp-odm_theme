b619a4a6fc1b6a1e0938982185a85da6442d64ff
Alex Corbi <a.corbi@gmail.com>
2016-06-22 17:56:20 +0200

    added data page template and 2 new widget areas for it

--- functions.php
+++ functions.php
@@ -89,6 +89,18 @@ function opendev_setup_theme()
     'before_title' => '<h2 class="widget-title">',
     'after_title' => '</h2>',
   ));
+    register_sidebar(array(
+    'name' => __('Data main', 'jeo'),
+    'id' => 'data-main',
+    'before_title' => '<h2 class="widget-title">',
+    'after_title' => '</h2>',
+  ));
+    register_sidebar(array(
+    'name' => __('Data sidebar', 'jeo'),
+    'id' => 'data-sidebar',
+    'before_title' => '<h2 class="widget-title">',
+    'after_title' => '</h2>',
+  ));
 
     include get_stylesheet_directory().'/inc/layers.php';
 }
