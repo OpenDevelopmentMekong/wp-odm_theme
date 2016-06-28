3ab33a95f1f3caa8f6afce960062f29a0bc1b99f
Alex Corbi <a.corbi@gmail.com>
2016-06-28 19:24:22 +0200

    Implementing

--- inc/templates/custom-post-grid-single.php
+++ inc/templates/custom-post-grid-single.php
@@ -3,7 +3,7 @@
 <div id="post-<?php echo $post->ID; ?>" class="three columns custom-post-item">
 	<a href="<?php echo get_permalink($post->ID); ?>" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title; ?></a></h3>
 	<div class="meta">
-			<?php show_date_and_source_of_the_post($post); ?>
+			<?php show_post_meta($post); ?>
 	</div>
 	<?php
 		$thumb_src = opendev_get_thumbnail($post->ID);
