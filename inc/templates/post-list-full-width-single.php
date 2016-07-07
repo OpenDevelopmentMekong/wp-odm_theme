d7ecd2c16d925555bc0fb72885dd65b62da619ae
DragonVirus <limonster.li@gmail.com>
2016-07-07 15:43:55 +0630

    Styling post-list-full-width-single

--- inc/templates/post-list-full-width-single.php
+++ inc/templates/post-list-full-width-single.php
@@ -2,13 +2,17 @@
 
 <div class="post-list-item">
 	<a class="post-list-item-title" href="#"><?php echo $post->post_title; ?></a>
-	<?php show_post_meta($post); ?>
 	<div class="post-list-item-content">
 		<?php
 			$thumb_src = opendev_get_thumbnail($post->ID, true);
 			if (isset($thumb_src)):
 				echo $thumb_src;
 			endif; ?>
-		<p><?php echo $post->post_content; ?></p>
+		<div class="post-list-content-wrapper">
+			<?php show_post_meta($post); ?>	
+			<p class="post-excerpt"><?php echo $post->post_excerpt; ?></p>
+			<span class="readmore"><a href="">(read more)</a></span>
+		</div>
+		
 	</div>
 </div>
