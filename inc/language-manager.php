ab8554e24bdcbc5891f4f42834adcaf1929763b6
Alex Corbi <a.corbi@gmail.com>
2016-07-17 00:30:05 +0200

    fixed errors

--- inc/language-manager.php
+++ inc/language-manager.php
@@ -7,7 +7,7 @@
 
 class Odm_Language_Manager {
 
-  var $odm_language_manager()->get_current_language() = 'en';
+  var $current_language = 'en';
 
 	function __construct() {
     add_action( 'after_setup_theme', array($this,'init_language_manager'));
@@ -17,13 +17,13 @@ class Odm_Language_Manager {
 
     if (function_exists('qtranxf_getLanguage')) {
       $local_lang = qtranxf_getLanguage();
-      $this->odm_language_manager()->get_current_language() = $local_lang;
+      $this->current_language = $local_lang;
     }
 
   }
 
-  function get_odm_language_manager()->get_current_language()(){
-    return $this->odm_language_manager()->get_current_language();
+  function get_current_language(){
+    return $this->current_language;
   }
 
   function echo_language_selectors(){
