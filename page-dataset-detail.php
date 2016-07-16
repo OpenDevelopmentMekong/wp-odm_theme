df575be3a809d94b121f8a955ff9273061537ca1
Alex Corbi <a.corbi@gmail.com>
2016-07-16 18:04:56 +0200

    fixed layout

--- page-dataset-detail.php
+++ page-dataset-detail.php
@@ -6,21 +6,19 @@
 <?php get_header(); ?>
 
 <?php if(have_posts()) : the_post(); ?>
-	<section id="content" class="single-post">
-
-		<section class="container">
-			<div class="row">
-				<div class="eleven columns">
-					<?php the_content(); ?>
-					<?php
-						if (isset($_GET['id'])):
-							echo do_shortcode('[wpckan_dataset_detail id="' . $_GET['id'] . '"]');
-						else:
-							echo "<p>" . _e('Please provide an id as parameter', 'wpckan') . "</p>";
-						endif;
-					?>
-				</div>
+	<section id="content" class="container single-post">
+		<div class="row">
+			<div class="eleven columns">
+				<?php the_content(); ?>
+				<?php
+					if (isset($_GET['id'])):
+						echo do_shortcode('[wpckan_dataset_detail id="' . $_GET['id'] . '"]');
+					else:
+						echo "<p>" . _e('Please provide an id as parameter', 'wpckan') . "</p>";
+					endif;
+				?>
 			</div>
+
 			<div class="four columns offset-by-one">
 				<aside id="sidebar">
 					<?php dynamic_sidebar('wpckan-dataset-detail-sidebar'); ?>
