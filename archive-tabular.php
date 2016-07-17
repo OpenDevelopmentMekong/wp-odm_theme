113549bacc186a131428a7f5a8c5ad86430beefe
Alex Corbi <a.corbi@gmail.com>
2016-07-17 11:39:53 +0200

    better headers

---
+++ archive-tabular.php
@@ -0,0 +1,39 @@
+<?php get_header();?>
+
+<div class="section-title main-title">
+
+	<section class="container">
+		<header class="row">
+			<div class="eight columns">
+				<h1><?php _e('Tabular pages','odm') ?></h1>
+			</div>
+      <div class="eight columns">
+				<?php get_template_part('section', 'query-actions'); ?>
+			</div>
+		</header>
+	</section>
+
+	<section class="container">
+
+		<div class="row">
+			<?php while (have_posts()) : the_post();
+				odm_get_template('post-grid-single-4-cols',array(
+					"post" => get_post()
+			),true);
+			endwhile; ?>
+		</div>
+
+	</section>
+
+  <section class="container">
+		<div class="row">
+			<div class="sixteen columns">
+				<?php odm_get_template('pagination',array(),true); ?>
+			</div>
+		</div>
+	</section>
+
+</div>
+
+
+<?php get_footer(); ?>
--- page-data.php
+++ page-data.php
@@ -6,39 +6,38 @@ Template Name: Data
 <?php get_header(); ?>
 
 <?php if (have_posts()) : the_post(); ?>
-	<section id="content" class="single-post data-page">
-		<header class="single-post-header">
-			<div class="container">
-				<div class="sixteen columns">
-					<h1><span class="icon-archive"></span> <?php the_title(); ?></h1>
-				</div>
+
+  <section class="container section-title main-title">
+		<header class="row">
+			<div class="sixteen columns">
+				<h1><?php the_title(); ?></h1>
 			</div>
 		</header>
-		<div class="container">
-			<div class="eleven columns">
-				<div class="six columns">
-					<?php dynamic_sidebar('data-main-left'); ?>
-				</div>
-				<div class="six columns">
-					<?php dynamic_sidebar('data-main-middle'); ?>
-				</div>
-				<div class="four columns">
-					<?php dynamic_sidebar('data-main-right'); ?>
-				</div>
-				<div class="sixteen columns">
-					<?php dynamic_sidebar('data-main-bottom'); ?>
-				</div>
-			</div>
-			<div class="four columns offset-by-one">
-				<aside id="sidebar">
-					<ul class="widgets">
-						<?php dynamic_sidebar('data-sidebar'); ?>
-					</ul>
-				</aside>
-			</div>
-
-		</div>
 	</section>
+
+	<section class="container">
+		<div class="eleven columns">
+			<div class="six columns">
+				<?php dynamic_sidebar('data-main-left'); ?>
+			</div>
+			<div class="six columns">
+				<?php dynamic_sidebar('data-main-middle'); ?>
+			</div>
+			<div class="four columns">
+				<?php dynamic_sidebar('data-main-right'); ?>
+			</div>
+			<div class="sixteen columns">
+				<?php dynamic_sidebar('data-main-bottom'); ?>
+			</div>
+		</div>
+		<div class="four columns offset-by-one">
+			<aside id="sidebar">
+				<ul class="widgets">
+					<?php dynamic_sidebar('data-sidebar'); ?>
+				</ul>
+			</aside>
+		</div>
+</section>
 <?php endif; ?>
 
 <?php get_footer(); ?>
