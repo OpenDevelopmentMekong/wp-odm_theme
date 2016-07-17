71e3adefc214da932b4db4011fa51ea7fb28220b
Alex Corbi <a.corbi@gmail.com>
2016-07-17 11:50:29 +0200

    better page template

--- page.php
+++ page.php
@@ -2,26 +2,26 @@
 
 <!-- //cookie -->
 <?php if(have_posts()) : the_post(); ?>
-	<section id="content" class="single-post">
-		<header class="single-post-header">
-			<div class="container">
-				<div class="sixteen columns">
-					<h1><?php the_title(); ?></h1>
-				</div>
+
+  <section class="container section-title main-title">
+		<header class="row">
+			<div class="sixteen columns">
+				<h1><?php the_title(); ?></h1>
 			</div>
 		</header>
-		<div class="container">
-			<div class="sixteen columns">
-				<?php the_content(); ?>
-				<?php
-				wp_link_pages( array(
-					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'jeo' ) . '</span>',
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-				) );
-				?>
-			</div>
+	</section>
+
+	<section class="container">
+		<div class="sixteen columns">
+			<?php the_content(); ?>
+			<?php
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'jeo' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+			) );
+			?>
 		</div>
 	</section>
 <?php endif; ?>
