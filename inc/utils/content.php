cadf30e1b87c0560570bc74eddb9c8bacedf6733
Alex Corbi <a.corbi@gmail.com>
2016-07-18 23:28:34 +0200

    get_the_time

--- inc/utils/content.php
+++ inc/utils/content.php
@@ -280,12 +280,12 @@ function echo_post_meta($post, $show_elements = array('date','sources','categori
   					 <?php
   					 if (function_exists('qtrans_getLanguage')) {
   							 if (odm_language_manager()->get_current_language() == 'km') {
-  									 echo convert_date_to_kh_date(get_the_modified_time('j.M.Y'),$post->ID);
+  									 echo convert_date_to_kh_date(get_the_time('j.M.Y'),$post->ID);
   							 } else {
-  									 echo get_the_modified_time('j F Y',$post->ID);
+  									 echo get_the_time('j F Y',$post->ID);
   							 }
   					 } else {
-  							 echo get_the_modified_time('j F Y',$post->ID);
+  							 echo get_the_time('j F Y',$post->ID);
   					 }
   				?>
   			</li>
