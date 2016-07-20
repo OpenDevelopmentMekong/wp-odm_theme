894956d33c6b4204815d77cb9d66a2e3a86737aa
Alex Corbi <a.corbi@gmail.com>
2016-07-20 14:52:21 +0200

    Fixes

--- inc/templates/post-list-single-1-cols.php
+++ inc/templates/post-list-single-1-cols.php
@@ -1,17 +1,21 @@
 <?php
 	$post = $params["post"];
 	$show_meta = $params["show_meta"];
+	$show_excerpt = $params["show_excerpt"];
 	?>
 
 <div class="sixteen columns post-list-item">
 	<p><a class="post-list-item-title" href="<?php echo get_permalink($post->ID); ?>" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title; ?></a></p>
 	<div class="post-list-item-content">
-		<?php
-			$thumb_src = odm_get_thumbnail($post->ID,false);
-			if (isset($thumb_src)):
-				echo $thumb_src;
-			endif; ?>
-			<div class="post-excerpt"><?php echo odm_excerpt($post); ?></div>
+			<?php if ($show_excerpt):
+				$thumb_src = odm_get_thumbnail($post->ID,false);
+				if (isset($thumb_src)):
+					echo $thumb_src;
+				endif; ?>
+				<div class="post-excerpt">
+					<?php echo odm_excerpt($post); ?>
+				</div>
+			<?php endif; ?>
 			<?php if ($show_meta): ?>
 			<div class="meta">
 					<?php echo_post_meta($post,array('date','sources','categories')); ?>
