cb027b86c78d85f425b5c19319e8883c190b92ab
Alex Corbi <a.corbi@gmail.com>
2016-07-22 10:58:31 +0200

    Fixes #167

--- inc/utils/content.php
+++ inc/utils/content.php
@@ -464,4 +464,45 @@ function available_custom_post_types(){
 		},get_the_category($post->ID));
  }
 
+ function odm_echo_extras(){
+
+   if (function_exists('get')) :
+       if (get('author') == '' && get('author'.odm_language_manager()->get_current_language()) == ''):
+         echo '';
+       else:
+         $news_source_info = '<span class="lsf">&#xE041;</span> ';
+         if (get('author'.odm_language_manager()->get_current_language()) != ''):
+             $news_source_info .= get('author'.odm_language_manager()->get_current_language()).'<br />';
+         else:
+             $news_source_info .= get('author').'<br />';
+         endif;
+       endif;
+   endif;
+
+   if (function_exists('get')):
+     if (get('article_link') == '' && get('article_link'.odm_language_manager()->get_current_language()) == ''):
+         echo '';
+     else:
+       if (get('article_link'.odm_language_manager()->get_current_language()) != ''):
+         $source = get('article_link'.odm_language_manager()->get_current_language());
+       else:
+         $source = get('article_link');
+       endif;
+     endif;
+
+     if (isset($source) && $source != ''):
+         if (substr($source, 0, 7) != 'http://'):
+           $news_source_info .= '<a href="http://'.$source.'" target="_blank">http://'.$source.'</a>';
+         else:
+           $news_source_info .= '<a href="'.$source.'" target="_blank">'.$source.'</a>';
+         endif;
+     endif;
+   endif;
+
+   if (isset($news_source_info) && $news_source_info != ''):
+     echo '<p>'.$news_source_info.'</p>';
+   endif;
+   
+ }
+
  ?>
--- single.php
+++ single.php
@@ -40,45 +40,7 @@
                 <?php endif; ?>
             <?php endif; ?>
             <?php the_content(); ?>
-            <?php
-                if (function_exists('get')) :
-                    if (get('author') == '' && get('author'.odm_language_manager()->get_current_language()) == ''):
-                      echo '';
-                    else:
-                      $news_source_info = '<span class="lsf">&#xE041;</span> ';
-                      if (get('author'.odm_language_manager()->get_current_language()) != ''):
-                          $news_source_info .= get('author'.odm_language_manager()->get_current_language()).'<br />';
-                      else:
-                          $news_source_info .= get('author').'<br />';
-                      endif;
-                    endif;
-                endif;
-             ?>
-            <?php
-                if (function_exists('get')):
-                  if (get('article_link') == '' && get('article_link'.odm_language_manager()->get_current_language()) == ''):
-                      echo '';
-                  else:
-                    if (get('article_link'.odm_language_manager()->get_current_language()) != ''):
-                      $source = get('article_link'.odm_language_manager()->get_current_language());
-                    else:
-                      $source = get('article_link');
-                    endif;
-                  endif;
-
-                  if (isset($source) && $source != ''):
-                      if (substr($source, 0, 7) != 'http://'):
-                        $news_source_info .= '<a href="http://'.$source.'" target="_blank">http://'.$source.'</a>';
-                      else:
-                        $news_source_info .= '<a href="'.$source.'" target="_blank">'.$source.'</a>';
-                      endif;
-                  endif;
-                endif;
-
-                if (isset($news_source_info) && $news_source_info != ''):
-                  echo '<p>'.$news_source_info.'</p>';
-                endif;
-                ?>
+            <?php odm_echo_extras(); ?>
             <?php
               wp_link_pages(array(
                       'before' => '<div class="page-links"><span class="page-links-title">'.__('Pages:', 'jeo').'</span>',
