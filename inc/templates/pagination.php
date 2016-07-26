66c8eacb5bf10ddf192a527b3ba0fda8f6c1c651
Mardy <mardy@odc.org>
2016-07-26 16:26:24 +0700

    styling

--- inc/templates/pagination.php
+++ inc/templates/pagination.php
@@ -4,16 +4,11 @@
 
 		$big = 999999999; // need an unlikely integer
 
-
 		echo paginate_links(array(
 				'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
 				'format' => '?paged=%#%',
 				'current' => max(1, $paged),
-				'prev_next' => ture,
 				'total' => $wp_query->max_num_pages,
-				//'prev_text' => '',
-				'before_page_number' => "&nbsp;",
-				'after_page_number' => "&nbsp;",
 		));
 	?>
 </div>
