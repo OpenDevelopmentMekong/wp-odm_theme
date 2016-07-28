0ecede78ab7d268402b7b8774e781b54da24be95
huyeng <huyeng_heng@yahoo.com>
2016-07-28 15:35:35 +0700

    Add the needed attributes for map catologue such as title_with_link and description (for grouping layers of subcategory)

--- inc/templates/post-grid-single-4-cols.php
+++ inc/templates/post-grid-single-4-cols.php
@@ -15,14 +15,29 @@
 			<?php
 		endif; ?>
 
-		<a class="item-title" href="<?php echo get_permalink($post->ID); ?>" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title; ?></a>
+		<?php
+		if (isset($post->title_and_link) && $post->title_and_link != "") :
+			echo $post->title_and_link;
+		else: ?>
+			<a class="item-title" href="<?php echo get_permalink($post->ID); ?>" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title; ?></a>
+			<?php 
+		endif; ?>
+
+		<?php
+		if (isset($post->description) && $post->description != "") :?>
+			<div class="post-grid-item-list">
+					<?php echo $post->description;   ?>
+			</div>
+			<?php
+		endif;
+		?>
 
 		<?php if ($show_meta): ?>
 			<div class="meta">
 					<?php echo_post_meta($post,array('date','sources','categories')); ?>
 			</div>
 		<?php endif; ?>
-		
+
 		<?php $thumb_src = odm_get_thumbnail($post->ID);
 		if (isset($thumb_src)):
 			echo $thumb_src;
