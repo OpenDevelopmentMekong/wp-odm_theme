3bb2bac20a210b6fb2627bf0b9496129895c4c52
huyeng <huyeng_heng@yahoo.com>
2016-08-03 14:03:43 +0700

    Updated title archive page of map layer

--- archive-map-layer.php
+++ archive-map-layer.php
@@ -5,7 +5,7 @@
 	<section class="container">
 		<header class="row">
 			<div class="eight columns">
-				<h1><?php _e('Map catalogue','odm') ?></h1>
+				<h1><?php _e('Maps catalogue','odm') ?></h1>
 			</div>
       <div class="eight columns">
 				<?php get_template_part('section', 'query-actions'); ?>
