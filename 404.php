e3afa8f7e7eb3bdc584c4d592fc8edd9ee09f9eb
Alex Corbi <a.corbi@gmail.com>
2016-08-24 17:11:28 +0200

    better layout

--- 404.php
+++ 404.php
@@ -6,14 +6,14 @@
 
   	<div class="container">
       <div class="row">
-        <div class="six columns offset-by-five align-center">
+        <div class="eight columns offset-by-four align-center">
           <h1><?php _e('Not found!','odm'); ?></h1>
           <i class="fa fa-map-signs fa-6" aria-hidden="true"></i>
     	  </div>
       </div>
 
       <div class="row">
-        <div class="six columns offset-by-five align-center">
+        <div class="eight columns offset-by-four align-center">
           <p><?php _e('If you received this error after selecting a new geographic location from the top menu, it means the new location doesn\'t have an information page on this topic that the old location had. To see what information does exist for this new location you\'ve chosen simply click any of the navigation main menu items (News, Topics, Maps, Data, etc..) from the navigation menu above this message .','odm'); ?></p>
           <p><a class="button" onClick="history.go(-1)"><?php _e('Back to the previous page','odm'); ?></a></p>
           <p><a class="button" href="/"><?php _e('Go to the current site\'s home','odm'); ?></a></p>
