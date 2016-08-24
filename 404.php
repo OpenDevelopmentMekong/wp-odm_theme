c257b1d83c0cc2a9cf995a5013d0e93e8b9768c6
Alex Corbi <a.corbi@gmail.com>
2016-08-24 17:16:23 +0200

    better layout

--- 404.php
+++ 404.php
@@ -14,6 +14,11 @@
       <div class="row">
         <div class="eight columns offset-by-four align-center">
           <p><?php _e('If you received this error after selecting a new geographic location from the top menu, it means the new location doesn\'t have an information page on this topic that the old location had. To see what information does exist for this new location you\'ve chosen simply click any of the navigation main menu items (News, Topics, Maps, Data, etc..) from the navigation menu above this message .','odm'); ?></p>
+    	  </div>
+      </div>
+
+      <div class="row">
+        <div class="eight columns offset-by-four align-center">
           <p><a class="button" onClick="history.go(-1)"><?php _e('Back to the previous page','odm'); ?></a></p>
           <p><a class="button" href="/"><?php _e('Go to the current site\'s home','odm'); ?></a></p>
           <p><a class="button" ><?php _e('Contact us','odm'); ?></a></p>
