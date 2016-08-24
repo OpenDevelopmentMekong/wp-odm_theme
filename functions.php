386b9fd9d5b798fde114aea97bceadd41e796d1b
huyeng <huyeng_heng@yahoo.com>
2016-08-24 10:56:04 +0700

    Updates profile areas.

--- functions.php
+++ functions.php
@@ -152,25 +152,19 @@ function odm_setup_theme()
     'name' => __('Profile area sidebar left', 'odm'),
     'id' => 'profile-area-1',
     'before_title' => '<h2 class="widget-title">',
-    'after_title' => '</h2>',
-    'before_widget' => '',
-    'after_widget'  => ''
+    'after_title' => '</h2>'
   ));
     register_sidebar(array(
     'name' => __('Profile area bottom left', 'odm'),
     'id' => 'profile-area-2',
     'before_title' => '<h2 class="widget-title">',
-    'after_title' => '</h2>',
-    'before_widget' => '',
-    'after_widget'  => ''
+    'after_title' => '</h2>'
   ));
     register_sidebar(array(
     'name' => __('Profile area bottom right', 'odm'),
     'id' => 'profile-area-3',
     'before_title' => '<h2 class="widget-title">',
-    'after_title' => '</h2>',
-    'before_widget' => '',
-    'after_widget'  => ''
+    'after_title' => '</h2>'
   ));
 
   include_once get_stylesheet_directory().'/inc/layers.php';
