1201aa4d2a180e76a088c32158cb049c30310c05
Alex Corbi <a.corbi@gmail.com>
2016-11-23 22:50:18 +0100

    Added first half of statistics

--- page-data.php
+++ page-data.php
@@ -149,28 +149,20 @@ Template Name: Data
 
       </form>
 
-      <?php
-        if (!$active_filters):
-          $shortcode = '[wpckan_number_of_query_datasets limit="1"';
-          if (isset($param_country) && $param_country != 'mekong'):
-            $shortcode .= ' filter_fields=\'{"extras_odm_spatial_range":"'. $countries[$param_country]['iso2'] . '"}\'';
-          endif;
-          ?>
-          <div class="sixteen columns">
-            <div class="data-number-results-small">
-              <p>
-                <p class="label"><label><?php _e('Current statistics: ','odm'); ?></label></p>
-                <?php echo do_shortcode($shortcode . ' type="dataset" suffix=" Datasets"]'); ?>
-                <?php echo do_shortcode($shortcode . ' type="library_record" suffix=" Library records"]'); ?>
-                <?php echo do_shortcode($shortcode . ' type="laws_record" suffix=" Laws"]'); ?>
-              </p>
-            </div>
-          </div>
-          <?php
-        endif; ?>
-
     </div>
   </div>
+
+  <?php
+    if (!$active_filters && function_exists('wpdash_get_ckan_stats_dataviz')): ?>
+      <div class="container">
+        <div class="row">
+          <div class="eight columns">
+            <?php wpdash_get_ckan_stats_dataviz(); ?>
+          </div>
+        </div>
+      </div>
+  <?php
+    endif; ?>
 
   <section class="container">
     <div class="row">
