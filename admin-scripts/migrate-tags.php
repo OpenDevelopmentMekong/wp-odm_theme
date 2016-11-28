97cccc7a3047eedd68709a55ab53402fe1991c7b
Alex Corbi <a.corbi@gmail.com>
2016-11-28 14:51:19 +0100

    Fixes #620

--- admin-scripts/migrate-tags.php
+++ admin-scripts/migrate-tags.php
@@ -13,69 +13,86 @@ $tags_by_metadata = array(
 
 if(!is_user_logged_in() && !isSiteAdmin()):
 
-  echo('You do not have access');
+  echo('You do not have access to this functionality');
 
 else:
 
-	echo "Getting posts by tags\n";
+	echo "Getting posts by tags" . nl2br("\n");
 
 	foreach ( $tags_by_metadata as $metadata_field => $config):
 
 		foreach ( $config["tags"] as $value):
 
-			echo("Looking for posts with tag:" . $value . "\n");
+			echo("Looking for posts with tag:" . $value . nl2br("\n"));
 
 			$args = array(
-		    'tag_id' => $value
+		    'tag' => $value
 			);
 
 			$posts = get_posts($args);
-			echo(count($posts) . "posts found with tag:" . $value . "\n");
+			echo(count($posts) . "posts found with tag:" . $value . nl2br("\n"));
 
 			foreach ( $posts as $post):
 
-				echo("Analizing post with title:" . $post->post_title . "\n");
+				echo("Analizing post with title:" . $post->post_title . nl2br("\n"));
+
+				echo("Looking for related_content metadata field" . nl2br("\n"));
 
 				$related_content = get_post_meta($post->ID,'related_content',false);
-				if (!empty($related_content)):
-					echo("related_content meta field found\n");
+				$metadata_found = false;
+				$related_content_object = array();
+				$related_content = empty($related_content) ? null : $related_content[0];
+
+				if ($related_content != null):
+					echo("related_content meta field found, checking for values..." . nl2br("\n"));
 					print_r($related_content);
-					echo("adapting...\n");
-					$related_content_object = json_decode($related_content[0]);
-					$metadata_found = false;
+
+					$related_content_object = json_decode($related_content);
 
 					foreach ( $related_content_object as $related_content):
-						if ($related_content["type"] === $metadata_field
-								&& $related_content["url"] === $config["prefix"] . $value):
+						if ($related_content->type === $metadata_field
+								&& $related_content->url === $config["prefix"] . $value):
 							$metadata_found = true;
 						endif;
+					endforeach;
+				endif;
 
-						if ($metadata_found == true):
-							echo("this metadata field is already on the related_content object...\n");
-						else:
-							echo("Adding metadata field to related_content object...\n");
-							array_push($related_content_object, array(
-								"type" => $metadata_field,
-								"url" => $config["prefix"] . $value
-							));
+				if ($metadata_found === false):
+					echo("Value not found. Setting related_content metadata field..." . nl2br("\n"));
+					array_push($related_content_object, array(
+						"type" => $metadata_field,
+						"url" => $config["prefix"] . $value
+					));
+
+					$related_content_json = json_encode($related_content_object);
+					update_post_meta($post->ID,'related_content',$related_content_json,$related_content);
+					print_r($related_content_json);
+				endif;
+
+				echo("Looking for " . $metadata_field . " metadata field" . nl2br("\n"));
+
+				$metadata = get_post_meta($post->ID,$metadata_field,false);
+				$value_available = false;
+
+				if (!empty($metadata)):
+
+					echo($metadata_field . " meta field found, checking for values..." . nl2br("\n"));
+					print_r($metadata);
+
+					foreach ($metadata as $metadata_value):
+						if ($metadata_value == $value):
+							$value_available = true;
 						endif;
 					endforeach;
 
-					echo("Adapted related_content object...\n");
-					$related_content_json = json_encode($related_content_object);
-					update_post_meta($post->ID,'related_content',$related_content_json,$related_content[0]);
-					print_r($related_content_json);
-				else:
-					echo("Setting related_content\n");
-
 				endif;
 
-				$metadata = get_post_meta($post->ID,$metadata_field,false);
-				if (!empty($related_content)):
-					print_r($related_content);
-				else:
-					add_post_meta($post->ID, 'odm_company', $value, false );
+				if ($value_available === false):
+					echo("Value not found. Setting " . $metadata_field . " metadata field" . nl2br("\n"));
+					add_post_meta($post->ID, $metadata_field, $value, false );
 				endif;
+
+				echo(nl2br("\n\n"));
 
 			endforeach;
 
