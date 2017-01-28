b68a98b4a5500b310a61f1494a92d3fec20a6dc5
huyeng <huyeng.heng8@gmail.com>
2017-01-28 19:45:10 +0700

    Updates to reduce conditions

--- functions.php
+++ functions.php
@@ -413,25 +413,27 @@ add_action('pre_get_posts', 'odm_return_all_topics' );
 
 function odm_search_pre_get_posts($query)
 {
-  if(get_post_type() && !is_admin()){
-    $post_type = $query->query['post_type'][0];
-  }else {
-    if (!is_admin() && ($query->is_search || get_query_var('odm_advanced_nav') || $query->is_tax || $query->is_category || $query->is_tag)) {
-      $query->set('post_type', available_post_types_search());
+  if(!is_admin()):
+    if(isset($query->query['post_type'])){
+      $query->set('post_type', $query->query['post_type']);
+    }else {
+      if (!is_admin() && ($query->is_search || get_query_var('odm_advanced_nav') || $query->is_tax || $query->is_category || $query->is_tag)) {
+        $query->set('post_type', available_post_types_search());
+      }
     }
-  }
-
+  endif;
 }
 add_action('pre_get_posts', 'odm_search_pre_get_posts');
 
 function odm_category_pre_get_posts($query)
 {
   if(!is_admin()):
-    if(get_post_type()){
-      $post_type = $query->query['post_type'][0];
+    if(isset($query->query['post_type'])){
+      $post_type = $query->query['post_type'];
     }else {
-        $post_type = isset($_GET['queried_post_type']) ? $_GET['queried_post_type'] : 'news-article';
+      $post_type = isset($_GET['queried_post_type']) ? $_GET['queried_post_type'] : 'news-article';
     }
+
     if ($query->is_category && isset($post_type)) {
         $query->set('post_type', array($post_type));
     }
