14c874ceb965b8d9f26be57e5ad573f772730e28
Alex Corbi <a.corbi@gmail.com>
2017-03-29 15:41:58 +0200

    using term_id

--- inc/widgets/odm-taxonomy-widget.php
+++ inc/widgets/odm-taxonomy-widget.php
@@ -44,7 +44,7 @@ class Odm_Taxonomy_Widget extends WP_Widget {
 	 */
 	public function print_category( $category, $current_page_slug ="") {
 
-		$args = array( 'category_name' => $category->name );
+		$args = array( 'category' => $category->term_id );
 		$posts_by_category = get_posts( $args );
 		print_r($posts_by_category);
 		$category_has_contents = count($posts_by_category) > 0;
