9aa3d443e303e62f6c2a50fe1819a96f0bcf889d
Alex Corbi <a.corbi@gmail.com>
2017-03-29 15:54:53 +0200

    Fixes #813

--- inc/widgets/odm-taxonomy-widget.php
+++ inc/widgets/odm-taxonomy-widget.php
@@ -44,7 +44,11 @@ class Odm_Taxonomy_Widget extends WP_Widget {
 	 */
 	public function print_category( $category, $current_page_slug ="") {
 
-		$args = array( 'category' => $category->cat_ID );
+		$args = array(
+			'category' => $category->cat_ID,
+			'posts_per_page'   => 1,
+			'post_status'      => 'publish'
+		);
 		$posts_by_category = get_posts( $args );
 		print_r($posts_by_category);
 		$category_has_contents = count($posts_by_category) > 0;
