fd3e6dcfeec880b7c08897043e14aaa0bfcc9a67
Alex Corbi <a.corbi@gmail.com>
2017-03-29 15:43:22 +0200

    logging

--- inc/widgets/odm-taxonomy-widget.php
+++ inc/widgets/odm-taxonomy-widget.php
@@ -46,7 +46,7 @@ class Odm_Taxonomy_Widget extends WP_Widget {
 
 		$args = array( 'category' => $category->term_id );
 		$posts_by_category = get_posts( $args );
-		print_r($posts_by_category);
+		print_r($category);
 		$category_has_contents = count($posts_by_category) > 0;
 
 		echo "<span class='nochildimage-".odm_country_manager()->get_current_country().$category->slug."'>";
