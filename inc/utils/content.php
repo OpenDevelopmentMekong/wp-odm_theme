c78f33b81b98716e0551e4bc858e04260a8fa31f
Alex Corbi <a.corbi@gmail.com>
2017-04-25 17:56:37 +0200

    better post type

--- inc/utils/content.php
+++ inc/utils/content.php
@@ -1,5 +1,35 @@
 <?php
 
+function get_post_type_icon_class($type){
+
+	$icon = "fa fa-database";
+	if ($type == "dataset"){
+		$icon = "fa fa-database";
+	}elseif ($type == "library_record"){
+		$icon = "fa fa-book";
+	}elseif ($type == "laws_record"){
+		$icon = "fa fa-gavel";
+	}elseif ($type == "agreement"){
+		$icon = "fa fa-handshake-o";
+	}elseif ($type == "map-layer"){
+		$icon = "fa fa-map-marker";
+	}elseif ($type == "news-article"){
+		$icon = "fa fa-newspaper-o";
+	}elseif ($type == "topic"){
+		$icon = "fa fa-list";
+	}elseif ($type == "profiles"){
+		$icon = "fa fa-briefcase";
+	}elseif ($type == "story"){
+		$icon = "fa fa-lightbulb-o";
+	}elseif ($type == "announcement"){
+		$icon = "fa fa-bullhorn";
+	}elseif ($type == "site-update"){
+		$icon = "fa fa-flag";
+	}
+
+	return $icon;
+}
+
 function get_post_or_page_id_by_title($title_str, $post_type = 'topic')
 {
 		global $wpdb;
