e246f57e338e8081893dc4871a8527255c3ecdb5
Alex Corbi <a.corbi@gmail.com>
2017-05-03 11:43:55 +0200

    reducing size of link-single tempalte

--- inc/templates/post-link-single-1-cols.php
+++ inc/templates/post-link-single-1-cols.php
@@ -5,14 +5,14 @@
 <div class="sixteen columns">
 	<div class="post-list-item single_result_container">
 		<p>
-			<h2>
+			<h3>
 				<?php
 					$localized_title = apply_filters('translate_text', $post->post_title, odm_language_manager()->get_current_language());
 				 ?>
 				<a class="item-title" href="<?php echo get_permalink($post->ID); ?>" title="<?php echo $localized_title; ?>">
 					<?php echo $localized_title; ?>
 				</a>
-			</h2>
+			</h3>
 		</p>
 	</div>
 </div>
