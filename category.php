3a28867961d6d427b99cbd2072697b015d70c03f
Alex Corbi <a.corbi@gmail.com>
2017-05-03 10:20:30 +0200

    better variable check

--- category.php
+++ category.php
@@ -2,8 +2,9 @@
 get_header();
 
 $options = get_option('odm_options');
+$template = isset($options['category_page_template']) ? $options['category_page_template'] : "default";
 
-if ($options['category_page_template'] == 'latest'):
+if ($template == 'latest'):
   include plugin_dir_path(__FILE__). 'inc/templates/category-page-latest.php';
 else:
   include plugin_dir_path(__FILE__). 'inc/templates/category-page-default.php';
