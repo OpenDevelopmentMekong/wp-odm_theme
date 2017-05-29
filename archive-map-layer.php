7d16cbded06922b43b2fd22a035e08446260833e
Alex Corbi <a.corbi@gmail.com>
2017-05-29 13:38:03 +0200

    harmonizing css

--- archive-map-layer.php
+++ archive-map-layer.php
@@ -3,104 +3,99 @@
 $options = get_option('odm_options');
 $date_to_show = isset($options['single_page_date']) ? $options['single_page_date'] : "created"; ?>
 
-<div class="section-title main-title">
+<section class="container section-title main-title">
+	<header class="row">
+		<div class="eight columns">
+			<h1><?php _e('Maps catalog','odm') ?></h1>
+		</div>
+    <div class="eight columns">
+			<?php get_template_part('section', 'query-actions'); ?>
+		</div>
+	</header>
+</section>
 
-	<section class="container">
-		<header class="row">
-			<div class="eight columns">
-				<h1><?php _e('Maps catalog','odm') ?></h1>
-			</div>
-      <div class="eight columns">
-				<?php get_template_part('section', 'query-actions'); ?>
-			</div>
-		</header>
-	</section>
+<section class="container">
+	<div class="row">
+		<div class="sixteen columns filter-container">
+			<div class="panel more-filters-content row">
+				<?php
+				$cat_baselayers = 'base-layers';
+				$cat_map_catalogue = 'map-catalogue';
+				$layer_category = 'layer-category';
 
-	<section class="container">
-		<div class="row">
-			<div class="sixteen columns filter-container">
-				<div class="panel more-filters-content row">
-					<?php
-					$cat_baselayers = 'base-layers';
-					$cat_map_catalogue = 'map-catalogue';
-					$layer_category = 'layer-category';
-
-					$term_baselayers = get_term_by('slug', $cat_baselayers, $layer_category);
-					$cat_baselayers_id =  $term_baselayers->term_id;
-					$term_map_catalogue = get_term_by('slug', $cat_map_catalogue, $layer_category);
-					$cat_map_catalogue_id =  $term_map_catalogue->term_id;
-					$filter_arg = array(
-															'search_box' => true,
-															'cat_selector' => true,
-															'con_selector' => false,
-															'date_rang' => true,
-															'post_type' => get_post_type(),
-															'taxonomy' => "layer-category",
-															'exclude_cat_id' => $cat_map_catalogue_id,
-															'depth' => 1
-														 );
-					odm_adv_nav_filters($filter_arg);
-					?>
-				</div>
+				$term_baselayers = get_term_by('slug', $cat_baselayers, $layer_category);
+				$cat_baselayers_id =  $term_baselayers->term_id;
+				$term_map_catalogue = get_term_by('slug', $cat_map_catalogue, $layer_category);
+				$cat_map_catalogue_id =  $term_map_catalogue->term_id;
+				$filter_arg = array(
+														'search_box' => true,
+														'cat_selector' => true,
+														'con_selector' => false,
+														'date_rang' => true,
+														'post_type' => get_post_type(),
+														'taxonomy' => "layer-category",
+														'exclude_cat_id' => $cat_map_catalogue_id,
+														'depth' => 1
+													 );
+				odm_adv_nav_filters($filter_arg);
+				?>
 			</div>
 		</div>
+	</div>
 
-    <div class="row">
-      <div class="sixteen columns">
-	      <?php
-				$search_string = isset($_GET['filter_s'])? $_GET['filter_s'] : null;
-				$filter_date_start = isset($_GET['filter_date_start'])? $_GET['filter_date_start'] : null;
-				$filter_date_end = isset($_GET['filter_date_end'])? $_GET['filter_date_end'] : null;
-				$filter_category = isset($_GET['filter_category'])? $_GET['filter_category'] : null;
+  <div class="row">
+    <div class="sixteen columns">
+      <?php
+			$search_string = isset($_GET['filter_s'])? $_GET['filter_s'] : null;
+			$filter_date_start = isset($_GET['filter_date_start'])? $_GET['filter_date_start'] : null;
+			$filter_date_end = isset($_GET['filter_date_end'])? $_GET['filter_date_end'] : null;
+			$filter_category = isset($_GET['filter_category'])? $_GET['filter_category'] : null;
 
-				$filter_post_type = isset($_GET['filter_post_type'])? $_GET['filter_post_type'] : null;
-				$filter_taxonomy = isset($_GET['filter_taxonomy'])? $_GET['filter_taxonomy'] : null;
-				$filter_layer = array(
-														'filter_s' => $search_string,
-														'filter_category' => $filter_category,
-														'filter_taxonomy' => $filter_taxonomy,
-														'filter_post_type' => $filter_post_type,
-														'filter_date_start' => $filter_date_start,
-														'filter_date_end' => $filter_date_end
-													);
+			$filter_post_type = isset($_GET['filter_post_type'])? $_GET['filter_post_type'] : null;
+			$filter_taxonomy = isset($_GET['filter_taxonomy'])? $_GET['filter_taxonomy'] : null;
+			$filter_layer = array(
+													'filter_s' => $search_string,
+													'filter_category' => $filter_category,
+													'filter_taxonomy' => $filter_taxonomy,
+													'filter_post_type' => $filter_post_type,
+													'filter_date_start' => $filter_date_start,
+													'filter_date_end' => $filter_date_end
+												);
 
-				//List cetegory and layer by cat for menu items
-				$map_catalogue = get_all_layers_grouped_by_subcategory(0, null, $filter_layer);
+			//List cetegory and layer by cat for menu items
+			$map_catalogue = get_all_layers_grouped_by_subcategory(0, null, $filter_layer);
 
-				$pagination = get_pagination_of_layers_grouped_by_subcategory($map_catalogue);
-				foreach ($map_catalogue as $key => $layer) {
-					$started_index = $key +1;
-					if($started_index >= $pagination["start_post"]):
-						odm_get_template('post-grid-single-4-cols',array(
-	            "post" => $layer,
-	            "show_meta" => false,
-							"meta_fields" => array("date"),
-							"view_large_image" => true,
-							"order" => $date_to_show
-							)
-	          , true);
-					endif;
+			$pagination = get_pagination_of_layers_grouped_by_subcategory($map_catalogue);
+			foreach ($map_catalogue as $key => $layer) {
+				$started_index = $key +1;
+				if($started_index >= $pagination["start_post"]):
+					odm_get_template('post-grid-single-4-cols',array(
+            "post" => $layer,
+            "show_meta" => false,
+						"meta_fields" => array("date"),
+						"view_large_image" => true,
+						"order" => $date_to_show
+						)
+          , true);
+				endif;
 
-					if($started_index == $pagination["end_post"]):
-						 break;
-					endif;
-				}
-			?>
-      </div>
-
+				if($started_index == $pagination["end_post"]):
+					 break;
+				endif;
+			}
+		?>
     </div>
 
-	</section>
+  </div>
 
-	<section class="container">
-		<div class="row">
-			<div class="sixteen columns">
-				<?php odm_get_template('pagination', array("paging_arg" => $pagination["paging_arg"]), true); ?>
-			</div>
+</section>
+
+<section class="container">
+	<div class="row">
+		<div class="sixteen columns">
+			<?php odm_get_template('pagination', array("paging_arg" => $pagination["paging_arg"]), true); ?>
 		</div>
-	</section>
-
-</div>
-
+	</div>
+</section>
 
 <?php get_footer(); ?>
--- archive.php
+++ archive.php
@@ -3,30 +3,28 @@ if(is_category()) {
 	include_once(get_stylesheet_directory() . '/category.php');
 	die();
 }
-get_header();
-?>
+get_header(); ?>
 
-<div class="section-title">
-	<div class="container">
-		<div class="sixteen columns">
-			<h1 class="archive-title"><?php
-					if( is_tag() || is_category() || is_tax() ) :
-						printf( __( '%s', 'odm' ), single_term_title() );
-					elseif ( is_day() ) :
-						printf( __( 'Daily Archives: %s', 'odm' ), get_the_date() );
-					elseif ( is_month() ) :
-						printf( __( 'Monthly Archives: %s', 'odm' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'odm' ) ) );
-					elseif ( is_year() ) :
-						printf( __( 'Yearly Archives: %s', 'odm' ), get_the_date( _x( 'Y', 'yearly archives date format', 'odm' ) ) );
-					elseif ( is_post_type_archive() ) :
-						_e(post_type_archive_title('', 0));
-					else :
-						_e( 'Archives', 'odm' );
-					endif;
-				?></h1>
-		</div>
+<div class="container section-title main-title">
+	<div class="sixteen columns">
+		<h1 class="archive-title"><?php
+				if( is_tag() || is_category() || is_tax() ) :
+					printf( __( '%s', 'odm' ), single_term_title() );
+				elseif ( is_day() ) :
+					printf( __( 'Daily Archives: %s', 'odm' ), get_the_date() );
+				elseif ( is_month() ) :
+					printf( __( 'Monthly Archives: %s', 'odm' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'odm' ) ) );
+				elseif ( is_year() ) :
+					printf( __( 'Yearly Archives: %s', 'odm' ), get_the_date( _x( 'Y', 'yearly archives date format', 'odm' ) ) );
+				elseif ( is_post_type_archive() ) :
+					_e(post_type_archive_title('', 0));
+				else :
+					_e( 'Archives', 'odm' );
+				endif;
+			?></h1>
 	</div>
 </div>
+
 <?php get_template_part('loop'); ?>
 
 <?php get_footer(); ?>
