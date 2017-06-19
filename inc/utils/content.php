8a6efdc1a0f984a821605fa94dd4c28d283396ce
Alex Corbi <a.corbi@gmail.com>
2017-06-19 18:56:54 +0200

    passing post id to get_the_tags

--- inc/utils/content.php
+++ inc/utils/content.php
@@ -414,7 +414,7 @@ function echo_post_meta($the_post, $show_elements = array('date','categories','t
       <?php
 			if (in_array('tags',$show_elements)): ?>
 					<?php
-					$tag_list = get_the_tags();
+					$tag_list = get_the_tags($post->ID);
 					if (isset($max_num_tags) && $max_num_tags > 0):
 						$tag_list = array_splice($tag_list,0,$max_num_tags);
 					endif;
