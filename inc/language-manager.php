0647a2e6ad388839202f25babfb6d34ff5c574fd
Alex Corbi <a.corbi@gmail.com>
2017-06-26 14:57:04 +0200

    fixed typo

--- inc/language-manager.php
+++ inc/language-manager.php
@@ -53,7 +53,7 @@ class Odm_Language_Manager {
 
 	function get_the_language_code_by_site($site = null){
     $theme = isset($site) ? $site : odm_country_manager()->get_current_country();
-    return $this->$languages_by_theme[$theme];
+    return $this->languages_by_theme[$theme];
   }
 
   function get_current_language(){
