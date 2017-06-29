be646d776532165340c433f2f02aad9ca7847df7
Alex Corbi <a.corbi@gmail.com>
2017-06-29 16:00:40 +0200

    Fixes #1042

--- inc/utils/content.php
+++ inc/utils/content.php
@@ -536,7 +536,8 @@ function echo_documents_cover ($postID = "") {
 
 function echo_downloaded_documents ($postID = "") {
 	$postID = $postID ? $postID : get_the_ID();
-	$local_lang = odm_language_manager()->get_current_language();
+	$local_lang = odm_language_manager()->get_the_language_code_by_site();
+	$current_lang = odm_language_manager()->get_current_language();
 	
 	//Get Download files
 	$get_document = get_post_meta($postID, 'upload_document', true);
@@ -544,7 +545,7 @@ function echo_downloaded_documents ($postID = "") {
 	
 	if (!empty($get_document) || !empty($get_localized_document)):		
 		if (!empty($get_document) && !empty($get_localized_document)):		
-			$document_curent_lang = $local_lang == "en" ? $get_document : $get_localized_document; 
+			$document_curent_lang = $current_lang == "en" ? $get_document : $get_localized_document; 
 		else:
 			$document_curent_lang = !empty($get_document) ? $get_document : $get_localized_document;
 		endif;?>
