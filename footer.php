4d29f4ebda22fc16f536b9a1384a44749ea8067c
Alex Corbi <a.corbi@gmail.com>
2017-09-11 11:05:13 +0200

    Fixes #1124

--- footer.php
+++ footer.php
@@ -76,15 +76,33 @@ jQuery(document).ready(function() {
     })
 });
 
-var _mare_pk = '30b24caf7c83f7a2eefb630abe4da0eb64b90de36d0eabeb12b0d3421c688e0f';
-var _mare_sc = '00fdba319ea2ad38cdce9fe19ea4bb7b';
+(function () {
 
-jQuery.getScript("https://mare.io/API/script.js", function(data, status, jqxhr) {
+    function loadScript(url, callback) {
 
-	/*
-		do something now that the script is loaded and code has been executed
-	*/
+        var script = document.createElement("script")
+        script.type = "text/javascript";
 
-});
+        if (script.readyState) { //IE
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" || script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else { //Others
+            script.onload = function () {
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+
+    loadScript("https://mare.io/API/script.js", function () {
+       console.log('mareio loaded');
+    });
+})();
 
 </script>
