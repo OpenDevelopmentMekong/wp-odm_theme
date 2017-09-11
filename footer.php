cdc40dad0b65d47ad9daca5ec11beff0aa38121a
Alex Corbi <a.corbi@gmail.com>
2017-09-11 10:37:27 +0200

    loading script with jQuery

--- footer.php
+++ footer.php
@@ -78,10 +78,13 @@ jQuery(document).ready(function() {
 
 var _mare_pk = '30b24caf7c83f7a2eefb630abe4da0eb64b90de36d0eabeb12b0d3421c688e0f';
 var _mare_sc = '00fdba319ea2ad38cdce9fe19ea4bb7b';
-(function() {
-  var mare = document.createElement('script'); mare.type = "text/javascript"; mare.async = true;
-  mare.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'mare.io/API/script.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mare, s);
-})();
+
+jQuery.getScript("https://mare.io/API/script.js", function(data, status, jqxhr) {
+
+	/*
+		do something now that the script is loaded and code has been executed
+	*/
+
+});
 
 </script>
