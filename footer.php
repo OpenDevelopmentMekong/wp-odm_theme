73c0900404b6d725ea96ab35aa9290f15658e6be
Alex Corbi <a.corbi@gmail.com>
2017-09-12 08:56:41 +0200

    Fixes #1124

--- footer.php
+++ footer.php
@@ -55,8 +55,8 @@
 </div>
 
 <script type="text/javascript">
-  var _mare_pk = '30b24caf7c83f7a2eefb630abe4da0eb64b90de36d0eabeb12b0d3421c688e0f';
-  var _mare_sc = '00fdba319ea2ad38cdce9fe19ea4bb7b';
+  var _mare_pk = '1e34dd25f7b430a1a7cb09454576b4612162fc56cff1ba7b4648e6fc9eacb7c4';
+  var _mare_sc = 'ee0c15de450f47a43b0070298881dc21';
   (function() {
     var mare = document.createElement('script'); mare.type = "text/javascript"; mare.async = true;
     mare.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'mare.io/API/script.js';
