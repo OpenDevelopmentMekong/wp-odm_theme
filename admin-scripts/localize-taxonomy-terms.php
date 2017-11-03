f2cfce46fac4b4fd05349f88c8ad2c6c56000de5
Alex Corbi <a.corbi@gmail.com>
2017-11-03 13:33:32 +0100

    Fixes #1223

--- admin-scripts/localize-taxonomy-terms.php
+++ admin-scripts/localize-taxonomy-terms.php
@@ -4,15 +4,20 @@ function isSiteAdmin(){
   return in_array('administrator',  wp_get_current_user()->roles);
 }
 
+$local_lang = odm_language_manager()->get_the_language_code_by_site();
+
 if(!is_user_logged_in() && !isSiteAdmin()):
 
   echo('You do not have access to this functionality');
 
+elseif(odm_country_manager()->get_current_country() == "mekong"):
+
+	echo('This operation cannot be executed on ODM site');
+
 else:
 
 	$qtranslate_term_name = get_option('qtranslate_term_name');
-	//print_r($qtranslate_term_name);
-	$local_lang = odm_language_manager()->get_the_language_code_by_site();
+
 	$localizations = odm_taxonomy_manager()->get_taxonomy_translations_for_lang($local_lang);
 
 	echo "Importing translations for taxonomy in: " . $local_lang . nl2br("\n");
