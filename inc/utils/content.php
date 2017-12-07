5f42f450c0e829ebf4ac56c497b722792c054bb8
Alex Corbi <a.corbi@gmail.com>
2017-12-07 12:24:22 +0100

    Fixes #1235

--- inc/utils/content.php
+++ inc/utils/content.php
@@ -778,16 +778,12 @@ function available_custom_post_types(){
 
 
  function get_top_level_category_english_names($cats) {
-		 $top_cat_names  = array();
-     foreach($cats as $cat) {
-			$all_parent_cats = get_category_parents($cat);
-			foreach($all_parent_cats as $parent_cat) {
-        if ($parent_cat->parent == 0) {
-					$top_cat_names[] = apply_filters('translate_text', get_cat_name($parent_cat->cat_ID), 'en');
-        }
-			}
-     }
-     return $top_cat_names;
+	$top_cat_names  = array();
+  foreach($cats as $cat) {
+		$all_parent_cats = get_category_parents($cat);
+		$top_cat_names = array_merge($top_cat_names,split('/', $all_parent_cats));
+  }
+  return $top_cat_names;
  }
 
  function odm_echo_extras($postID = "") {
