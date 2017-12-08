a49fe1cf513fec2e0f657b9c6fe9f2ab1fe64305
Alex Corbi <a.corbi@gmail.com>
2017-12-08 14:39:16 +0100

    FIxes #1235

--- inc/utils/arrays.php
+++ inc/utils/arrays.php
@@ -3,7 +3,7 @@
 	function arrays_have_common_items($array1, $array2) {
 
 		foreach ($array1 as $key => $value):
-			if (preg_grep( "/ . $value . /i" , $array2 )):
+			if (preg_grep( "/" . $value . "/i" , $array2 )):
 				return true;
 			endif;
 		endforeach;
